46bf1ad979f783a71163c9bfafd7dbe578be1a65
Patric Boscolo <patric@imenja.io>
2022-01-30 20:03:18 +0100

    Hello World of colly - checking dependencies - go configuration

---
+++ src/cloud/data/ojp/bacrawler.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		err := e.Request.Visit(e.Attr("href"))
+		if err != nil {
+			return
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	err := c.Visit("http://go-colly.org/")
+	if err != nil {
+		return
+	}
+}
