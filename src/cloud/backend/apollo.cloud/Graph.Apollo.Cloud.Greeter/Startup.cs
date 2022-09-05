c2a47c689037980a9f45d878cf865ab1ae051ecf
Patric Boscolo <patric@imenja.io>
2022-09-05 16:09:20 +0200

    implemented greeter service - integration test - unit test

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Greeter/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Graph.Apollo.Cloud.Greeter.Services;
+
+namespace Graph.Apollo.Cloud.Greeter
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc(o => o.EnableDetailedErrors = true);
+            services.AddSingleton<IGreeter, Services.Greeter>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+            });
+        }
+    }
+}
