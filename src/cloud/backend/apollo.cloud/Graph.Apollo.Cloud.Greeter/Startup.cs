3f0411cc2d1f753498b92fde9a974ed12ba53c10
Patric Boscolo <patric@imenja.io>
2022-09-06 02:44:45 +0200

    added application insights

--- src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Greeter/Startup.cs
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Greeter/Startup.cs
@@ -1,10 +1,16 @@
 ﻿using Graph.Apollo.Cloud.Greeter.Services;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
 
 namespace Graph.Apollo.Cloud.Greeter
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc(o => o.EnableDetailedErrors = true);
@@ -13,6 +19,7 @@ namespace Graph.Apollo.Cloud.Greeter
             {
                 o.Services.MapService("greet.Greeter", r => r.Tags.Contains("greeter"));
             });
+            services.AddApplicationInsightsTelemetry(Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
