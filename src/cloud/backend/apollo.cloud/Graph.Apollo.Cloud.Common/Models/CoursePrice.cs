938ab126abd05f1f149d9e7d49b386e9c6af2aec
Patric Boscolo <patric@imenja.io>
2022-09-16 01:13:11 +0200

    Added course draft for further discussion

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Common/Models/CoursePrice.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using ProtoBuf.Grpc;
+
+namespace Graph.Apollo.Cloud.Common.Models;
+
+[DataContract]
+public class CoursePrice
+{
+    [DataMember(Order = 1)]
+    public decimal Price { get; set; }
+    [DataMember(Order=2)]
+    public DateTime StartDate { get; set; }
+    [DataMember(Order = 3)]
+    public DateTime? EndDate { get; set; }
+}
+
