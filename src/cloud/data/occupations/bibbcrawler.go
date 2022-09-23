7e4055ba78f9ae96b10377dc6e38446c1cc1178e
Patric Boscolo <patric@imenja.io>
2022-09-23 16:48:38 +0200

    added course definition

--- src/cloud/data/occupations/bibbcrawler.go
+++ src/cloud/data/occupations/bibbcrawler.go
@@ -3,21 +3,49 @@
 This crawler allows us to collect the occupations as stated on the bibb websites:
 [bibb apprenticeship search](https://www.bibb.de/dienst/berufesuche/de/index_berufesuche.php/search)
 
+Check out colly documentation for more information
+https://pkg.go.dev/github.com/gocolly/colly#HTMLElement
 */
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gocolly/colly"
+	"log"
+	"os"
+	"time"
 )
 
-type Fact struct {
+//TODO: Declare LimitRule
+type LimitRule struct {
+	// DomainRegexp is a regular expression to match against domains
+	DomainRegexp string
+	// DomainRegexp is a glob pattern to match against domains
+	DomainGlob string
+	// Delay is the duration to wait before creating a new request to the matching domains
+	Delay time.Duration
+	// RandomDelay is the extra randomized duration to wait added to Delay before creating a new request
+	RandomDelay time.Duration
+	// Parallelism is the number of the maximum allowed concurrent requests of the matching domains
+	Parallelism int
+	// contains filtered or unexported fields
+}
+
+type BibbOccupations struct {
 	//TODO: Define Fact struct for bibb
+}
+
+type WebPage struct {
+	Url     string `json:"url"`
+	Date    string `json:"date"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 func main() {
 	//create factlist
-	//allFacts := make([]Fact, 0)
+	var webPage = WebPage{}
 
 	//TODO: crawl the list of occupations relevant for INVITE project apollo
 
@@ -27,16 +55,48 @@ func main() {
 	//colly.AllowedDomains("bibb.de"),
 	)
 
-	collectoccupations.OnHTML("title", func(element *colly.HTMLElement) {
-		fmt.Println(element.Text)
+	collectoccupations.OnHTML("head title", func(element *colly.HTMLElement) {
+		//fmt.Println(element.Text)
+		webPage.Title = element.Text
+
+	})
+
+	collectoccupations.OnHTML("html body", func(element *colly.HTMLElement) {
+		//fmt.Println(element.Text)
+		webPage.Content = element.Text
+	})
+
+	collectoccupations.OnHTML("c-accentuation-box__content", func(htmlElement *colly.HTMLElement) {
+		fmt.Println("dropping occupations")
+		htmlElement.ForEach("li", func(i int, listElement *colly.HTMLElement) {
+			println(listElement.Text)
+		})
+		fmt.Println("done dropping occupations")
 	})
 
 	collectoccupations.OnRequest(func(request *colly.Request) {
+		webPage.Url = request.URL.String()
 		fmt.Println("Visiting: ", request.URL.String())
+	})
+
+	//TODO: crawl all the occupations on the list
+	//TODO: Create a list of Occupations to crawl -> div class c-accentuation-box__content contains ul with links to occupations
+	//get response from website
+	collectoccupations.OnResponse(func(response *colly.Response) {
+		//TODO: Statistics for AppInsights like pageCount++
+		log.Println(response.Headers)
+	})
+
+	//done loading index page
+	collectoccupations.OnScraped(func(response *colly.Response) {
+		//TODO: Store on Azure
+
+		//TODO: Save to specific blob
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", " ")
+		enc.Encode(webPage)
 	})
 
 	//TODO: create search URLs
 	collectoccupations.Visit("https://www.bibb.de/dienst/berufesuche/de/index_berufesuche.php/apprenticeships/?")
-
-	//TODO: crawl all the occupations on the list
 }
