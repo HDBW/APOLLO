f2c857735124f2044232d89bfabc6839908f49d1
Lars Heinrich <lh@heires.net>
2022-09-27 14:51:43 +0200

    added azure b2c infrastructure

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Platforms/Android/MainActivity.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Platforms/Android/MainActivity.cs
@@ -1,9 +1,16 @@
 ﻿namespace De.HDBW.Apollo.Client;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
+using Microsoft.Identity.Client;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
+    {
+        base.OnActivityResult(requestCode, resultCode, data);
+        AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+    }
 }
---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Services/BaseAuthService.cs
@@ -0,0 +1,54 @@
+﻿namespace De.HDBW.Apollo.Client.Services
+{
+    using De.HDBW.Apollo.Client.Contracts;
+    using De.HDBW.Apollo.Client.Models;
+    using Microsoft.Identity.Client;
+
+    public abstract class BaseAuthService : IAuthService
+    {
+        private readonly IPublicClientApplication authenticationClient;
+
+        protected BaseAuthService(IPublicClientApplication authenticationClient)
+        {
+            this.authenticationClient = authenticationClient;
+        }
+
+        public Task<AuthenticationResult?> SignInInteractively(CancellationToken cancellationToken)
+        {
+            return this.authenticationClient
+                   .AcquireTokenInteractive(Constants.Scopes)
+#if WINDOWS
+				.WithUseEmbeddedWebView(false)				
+#endif
+                   .ExecuteAsync(cancellationToken);
+        }
+
+        public async Task<AuthenticationResult?> AcquireTokenSilent(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var accounts = await this.authenticationClient.GetAccountsAsync(Constants.SignInPolicy);
+                var firstAccount = accounts.FirstOrDefault();
+                if (firstAccount is null)
+                {
+                    return null;
+                }
+
+                return await this.authenticationClient.AcquireTokenSilent(Constants.Scopes, firstAccount).ExecuteAsync(cancellationToken);
+            }
+            catch (MsalUiRequiredException)
+            {
+                return null;
+            }
+        }
+
+        public async Task LogoutAsync(CancellationToken cancellationToken)
+        {
+            var accounts = await this.authenticationClient.GetAccountsAsync();
+            foreach (var account in accounts)
+            {
+                await this.authenticationClient.RemoveAsync(account);
+            }
+        }
+    }
+}
