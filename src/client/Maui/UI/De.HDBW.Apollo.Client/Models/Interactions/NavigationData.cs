4dbccfd9a9ccebeb76c354ee81d7d27649fe61ad
Lars Heinrich <lh@heires.net>
2022-10-06 18:22:18 +0200

    improved logging, added interaction on StartView Created AssesmentView, registration and routes updated nugets.

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Models/Interactions/NavigationData.cs
@@ -0,0 +1,9 @@
+﻿namespace De.HDBW.Apollo.Client.Models.Interactions
+{
+    public record NavigationData(string route, NavigationParameters? parameters)
+    {
+        public string Route { get; init; } = route;
+
+        public NavigationParameters? Parameters { get; init; } = parameters;
+    }
+}
---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/AssessmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using De.HDBW.Apollo.Client.Contracts;
+using Microsoft.Extensions.Logging;
+
+namespace De.HDBW.Apollo.Client.ViewModels
+{
+    public class AssessmentViewModel : BaseViewModel
+    {
+        public AssessmentViewModel(
+            IDispatcherService dispatcherService,
+            INavigationService navigationService,
+            IDialogService dialogService,
+            ILogger<AssessmentViewModel> logger)
+            : base(dispatcherService, navigationService, dialogService, logger)
+        {
+        }
+    }
+}
