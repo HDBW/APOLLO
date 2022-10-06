8bbfceb42a2d4b7cd11f1e13adae1eb18eb90747
Patric Boscolo <patric@imenja.io>
2022-10-07 01:34:58 +0200

    Seems to work with interfaces and protobuf client. Maybe need to add type sync as well? see https://stackoverflow.com/questions/71420086/protobuf-net-grpc-serializing-interfaces

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Program.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The apollo project licenses this file to you under the MIT license.
+
+namespace Graph.Apollo.Cloud.Assessment;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
