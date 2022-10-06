081e0f3a4a8344367d992abf81164c0c9d8406ab
Lars Heinrich <lh@heires.net>
2022-10-06 21:18:43 +0200

    Proto implementation of Assesments

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Selector/QuestionTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿namespace De.HDBW.Apollo.Client.Selector
+{
+    public class QuestionTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate? UniformGridTemplate { get; set; }
+
+        public DataTemplate? HorizontalListTemplate { get; set; }
+
+        public DataTemplate? OverlayTemplate { get; set; }
+
+        public DataTemplate? CompareTemplate { get; set; }
+
+        public DataTemplate? DefaultTemplate { get; set; }
+
+        protected override DataTemplate? OnSelectTemplate(object item, BindableObject container)
+        {
+            switch (item)
+            {
+                case string:
+                    return UniformGridTemplate;
+                case int:
+                    return HorizontalListTemplate;
+                case Visibility:
+                    return OverlayTemplate;
+                case FlowDirection:
+                    return CompareTemplate;
+                default:
+                    return DefaultTemplate;
+            }
+        }
+    }
+}
--- src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/AssessmentViewModel.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/AssessmentViewModel.cs
@@ -1,10 +1,19 @@
-﻿using De.HDBW.Apollo.Client.Contracts;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using De.HDBW.Apollo.Client.Contracts;
 using Microsoft.Extensions.Logging;
 
 namespace De.HDBW.Apollo.Client.ViewModels
 {
-    public class AssessmentViewModel : BaseViewModel
+    public partial class AssessmentViewModel : BaseViewModel
     {
+        [ObservableProperty]
+        private ObservableCollection<object> _questions = new ObservableCollection<object>();
+
+        [ObservableProperty]
+        private object? _currentQuestion;
+
         public AssessmentViewModel(
             IDispatcherService dispatcherService,
             INavigationService navigationService,
@@ -12,6 +21,24 @@ namespace De.HDBW.Apollo.Client.ViewModels
             ILogger<AssessmentViewModel> logger)
             : base(dispatcherService, navigationService, dialogService, logger)
         {
+            _questions = new ObservableCollection<object>()
+            {
+                "test",
+                1,
+                Visibility.Visible,
+                FlowDirection.MatchParent,
+                double.NaN
+            };
+
+            CurrentQuestion = Questions.FirstOrDefault();
+        }
+
+        [RelayCommand]
+        public void NextQuestion()
+        {
+            var currentIndex = Questions.IndexOf(CurrentQuestion);
+            currentIndex = currentIndex + 1 >= Questions.Count ? 0 : currentIndex + 1;
+            CurrentQuestion = Questions[currentIndex];
         }
     }
 }
