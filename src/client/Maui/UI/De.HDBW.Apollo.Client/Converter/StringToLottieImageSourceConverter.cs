49dab9e94922891c32416065560908ee843e8f4f
Lars Heinrich <lh@heires.net>
2022-10-06 21:18:43 +0200

    Proto implementation of Assesments

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Converter/StringToLottieImageSourceConverter.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Converter/StringToLottieImageSourceConverter.cs
@@ -28,7 +28,7 @@ namespace De.HDBW.Apollo.Client.Converter
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotSupportedException("TwoWayBinding not supported");
+            throw new NotSupportedException($"Binding two way is not supported in {GetType()}");
         }
     }
 }
