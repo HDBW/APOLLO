91a23d1ca7b8abb2041da1141fd9644ad9a6cd13
Patric Boscolo <patric@imenja.io>
2022-10-13 15:41:46 +0200

    AssessmentTest currently producing errors.

--- src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Services/AssessmentServer.cs
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Services/AssessmentServer.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Graph.Apollo.Cloud.Common.Models.Assessment;
+using Graph.Apollo.Cloud.Common.Models.Taxonomy;
 
 namespace Graph.Apollo.Cloud.Assessment.Services;
 
@@ -18,9 +19,9 @@ public class AssessmentService : IAssessmentService
     {
         var result = new AssessmentResult {Assessments = new List<Common.Models.Assessment.Assessment>
             {
-                new Common.Models.Assessment.Assessment(){Value = new AssessmentItem(){Id = 0,Ticks = DateTime.Now.Ticks, Title = "Hello"}},
-                new Common.Models.Assessment.Assessment(){Value = new AssessmentItem(){Id = 0,Ticks = DateTime.Now.Ticks, Title = "World"}},
-                new Common.Models.Assessment.Assessment(){Value = new AssessmentItem(){Id = 0,Ticks = DateTime.Now.Ticks, Title = "Test"}}
+                new Common.Models.Assessment.Assessment(){Value = new AssessmentItem(){Id = 0,Ticks = DateTime.Now.Ticks, Title = "Hello 1"},Occupation = new Occupation(){Id = 1,Name = "Landschaftsshit",Schema = "http://data.europa.eu/esco/occupation/a10eb17a-3c78-4f7a-a1da-8f31146339d3"}},
+                new Common.Models.Assessment.Assessment(){Value = new AssessmentItem(){Id = 0,Ticks = DateTime.Now.Ticks, Title = "Hello 3"},Occupation = new Occupation(){Id = 1,Name = "Landschaftsshit",Schema = "http://data.europa.eu/esco/occupation/a10eb17a-3c78-4f7a-a1da-8f31146339d3"}},
+                new Common.Models.Assessment.Assessment(){Value = new AssessmentItem(){Id = 0,Ticks = DateTime.Now.Ticks, Title = "Hello 2"},Occupation = new Occupation(){Id = 1,Name = "Landschaftsshit",Schema = "http://data.europa.eu/esco/occupation/a10eb17a-3c78-4f7a-a1da-8f31146339d3"}},
             }
         };
         return new ValueTask<AssessmentResult>(result);
