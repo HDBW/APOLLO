72596aa89ef14bb91da9eaf4ac28b139021990ea
Patric Boscolo <patric@imenja.io>
2022-10-13 19:10:07 +0200

    fixed header

--- src/cloud/backend/apollo.cloud/AssessmentsClient/Program.cs
+++ src/cloud/backend/apollo.cloud/AssessmentsClient/Program.cs
@@ -1,6 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
+﻿
 using Graph.Apollo.Cloud.Common.Models.Assessment;
 using Graph.Apollo.Cloud.Common.Models.Taxonomy;
 using Grpc.Core;
