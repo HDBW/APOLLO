4f48da666dd1e07498a1d39529e7ea532466d701
Patric Boscolo <patric@imenja.io>
2022-10-15 02:46:48 +0200

    Added Schema for Course

--- src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/Course/CourseAppointment.cs
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/Course/CourseAppointment.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
 using System.Text;
 
 namespace Invite.Apollo.App.Graph.Common.Models.Course
 {
     [DataContract]
-    public class CourseAppointment
+    public class CourseAppointment : IEntity, IApolloGraphItem
     {
+        [Key]
+        [DataMember(Order = 1,IsRequired = true)]
+        public long Id { get; set; }
 
+        [DataMember(Order = 2)]
+        public long Ticks { get; set; }
+
+        [DataMember(Order = 3)]
+        [ForeignKey(nameof(CourseItem))]
+        public long CourseId { get; set; }
+
+        [DataMember(Order = 4,IsRequired = true)]
+        public string BackendId { get; set; }
+
+        [DataMember(Order = 5)]
+        public Uri Schema { get; set; }
     }
 }
--- src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/Course/CourseItem.cs
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/Course/CourseItem.cs
@@ -1,12 +1,17 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace Invite.Apollo.App.Graph.Common.Models.Course
 {
     [DataContract]
-    public class CourseItem : IEntity
+    public class CourseItem : IEntity, IApolloGraphItem
     {
         #region client stuff
-
+        [Key]
         [DataMember(Order = 1)]
         public long Id { get; set; }
 
@@ -15,7 +20,81 @@ namespace Invite.Apollo.App.Graph.Common.Models.Course
 
         #endregion
 
+        #region cloud stuff
+
+        [DataMember(Order = 3,IsRequired = true)]
+        public string BackendId { get; set; }
+
+        [DataMember(Order = 4)]
+        public Uri Schema { get; set; }
+
+        #endregion
+
+        public CourseType Type { get; set; }
+
+        #region relations
+
+        [ForeignKey(nameof(EduProvider))]
+        public long TrainingProviderId { get; set; }
+
+        [ForeignKey(nameof(EduProvider))]
+        public long CourseProviderId { get; set; }
+
+        public string TrainingProviderIdBackend { get; set; }
+        public string CourseProviderIdBackend { get; set; }
+
+        public CourseAvailability Availability { get; set; }
+
+        //TODO: QualificationProvider
+
+        [ForeignKey(nameof(CourseItem))]
+        public long? CoursePredecessor { get; set; }
+
+        public string? CoursePredecessorBackendId { get; set; }
+
+        [ForeignKey(nameof(CourseItem))]
+        public long? Successor { get; set; }
+
+        public string? CourseSuccessorId { get; set; }
+        
+        [ForeignKey(nameof(CourseItem))]
+        public long? CourseParent { get; set; }
+
+        public string? CourseParentId { get; set; }
+
+        #endregion
+    }
+
+    [DataContract]
+    public class CourseRequest : ICorrelationId
+    {
+        [DataMember(Order = 1, IsRequired = true)]
+        public string CorrelationId { get; set; }
+    }
+
+    [DataContract]
+    public class CourseResponse : ICorrelationId
+    {
+        [DataMember(Order = 1,IsRequired = true)]
+        public string CorrelationId { get; set; }
 
 
+        public Collection<CourseItem> Courses { get; set; }
+
+        public Collection<CourseModules> CourseModules { get; set; }
+    }
+
+    [DataContract]
+    public class CourseSearchRequest : ICorrelationId
+    {
+        [DataMember(Order = 1,IsRequired = true)]
+        public string CorrelationId { get; set; }
+    }
+
+    [DataContract]
+    public class CourseSearchResponse : ICorrelationId
+    {
+        [DataMember(Order = 1, IsRequired = true)]
+        public string CorrelationId { get; set; }
     }
 }
