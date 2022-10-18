dabb3b55e5b6ede98b1dec443d79d6ba0c7e88d0
Patric Boscolo <patric@imenja.io>
2022-10-18 03:31:55 +0200

    Updated Course Schema

---
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/ApolloLabel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Invite.Apollo.App.Graph.Common.Models
+{
+    public class ApolloLabel : IEntity, IApolloGraphItem
+    {
+        #region client stuff
+        [Key]
+        [DataMember(Order = 1)]
+        public long Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public long Ticks { get; set; }
+
+        #endregion
+
+        #region cloud stuff
+
+        [DataMember(Order = 3, IsRequired = true)]
+        public string BackendId { get; set; } = null!;
+
+        [DataMember(Order = 4)]
+        public Uri Schema { get; set; } = null!;
+
+        #endregion
+
+        [DataMember(Order = 5, IsRequired = true)]
+        public string Value { get; set; } = null!;
+
+        [DataMember(Order = 6, IsRequired = true)]
+        public string ColorCode { get; set; } = null!; 
+    }
+}
--- src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/Course/CourseApolloLabels.cs
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/Course/CourseApolloLabels.cs
@@ -5,16 +5,10 @@ using System.Runtime.Serialization;
 
 namespace Invite.Apollo.App.Graph.Common.Models.Course
 {
-    /// <summary>
-    /// A course can be part of a other course.
-    /// (In this case the actual course is a module of a course)
-    /// Since a module can be part of more than one course this is a reference
-    /// to a list of Unique Identifiers of the parent courses.
-    /// </summary>
-    [DataContract]
-    public class CourseModules : IEntity, IApolloGraphItem
+    public class CourseApolloLabels : IEntity, IApolloGraphItem
     {
-        #region client stuff
+        #region Implementation of IEntity
+
         [Key]
         [DataMember(Order = 1)]
         public long Id { get; set; }
@@ -24,7 +18,8 @@ namespace Invite.Apollo.App.Graph.Common.Models.Course
 
         #endregion
 
-        #region cloud stuff
+
+        #region Implementation of IApolloGraphItem
 
         [DataMember(Order = 3, IsRequired = true)]
         public string BackendId { get; set; } = null!;
@@ -34,14 +29,10 @@ namespace Invite.Apollo.App.Graph.Common.Models.Course
 
         #endregion
 
-        [DataMember(Order=5,IsRequired = true)]
         [ForeignKey(nameof(CourseItem))]
         public long CourseId { get; set; }
 
-
-        [DataMember(Order = 7,IsRequired = true)]
-        [ForeignKey(nameof(CourseItem))]
-        public long ModuleId { get; set; }
-
+        [ForeignKey(nameof(ApolloLabel))]
+        public long LabelId { get; set; }
     }
 }
