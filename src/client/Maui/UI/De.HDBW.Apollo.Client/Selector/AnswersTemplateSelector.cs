372d2c840613efb55ac48a657f178ff5985da3b9
Larhei <lh@heires.net>
2022-10-24 23:59:27 +0200

    Improved rendering of answers in assessments

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Selector/AnswersTemplateSelector.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="AnswersTemplateSelector.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using De.HDBW.Apollo.Client.Models.Assessment;
+
+namespace De.HDBW.Apollo.Client.Selector
+{
+    public class AnswersTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate? DefaultTemplate { get; set; }
+
+        public DataTemplate? ImageTemplate { get; set; }
+
+        public DataTemplate? ImageWithTextTemplate { get; set; }
+
+        public DataTemplate? PointTemplate { get; set; }
+
+        protected override DataTemplate? OnSelectTemplate(object item, BindableObject container)
+        {
+            var entry = item as AnswerEntry;
+            if (entry != null)
+            {
+                if (entry.HasPoint)
+                {
+                    return PointTemplate;
+                }
+
+                if (entry.HasImage && entry.HasText)
+                {
+                    return ImageWithTextTemplate;
+                }
+
+                if (entry.HasImage)
+                {
+                    return ImageTemplate;
+                }
+            }
+
+            return DefaultTemplate;
+        }
+    }
+}
