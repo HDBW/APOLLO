8cb38444132b9ec71b5f08780dc5a8a4718b9dc0
Larhei <lh@heires.net>
2022-10-24 23:59:27 +0200

    Improved rendering of answers in assessments

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Models/Assessment/AnswerEntry.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Models/Assessment/AnswerEntry.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Invite.Apollo.App.Graph.Common.Models;
 using Invite.Apollo.App.Graph.Common.Models.Assessment;
+using Invite.Apollo.App.Graph.Common.Models.Assessment.Enums;
 
 namespace De.HDBW.Apollo.Client.Models.Assessment
 {
@@ -16,6 +17,7 @@ namespace De.HDBW.Apollo.Client.Models.Assessment
     {
         private AnswerItem _answerItem;
         private IEnumerable<MetaDataItem> _answerMetaDataItems;
+        private Point? _point;
 
         private AnswerEntry(AnswerItem answerItem, IEnumerable<MetaDataItem> answerMetaDataItems)
         {
@@ -23,6 +25,82 @@ namespace De.HDBW.Apollo.Client.Models.Assessment
             ArgumentNullException.ThrowIfNull(answerMetaDataItems);
             _answerItem = answerItem;
             _answerMetaDataItems = answerMetaDataItems;
+        }
+
+        public bool HasText
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Text);
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return _answerMetaDataItems.FirstOrDefault(m => m.Type == MetaDataType.Text)?.Value ?? string.Empty;
+            }
+        }
+
+        public bool HasImage
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Image);
+            }
+        }
+
+        public string Image
+        {
+            get
+            {
+                return _answerMetaDataItems.FirstOrDefault(m => m.Type == MetaDataType.Image)?.Value ?? string.Empty;
+            }
+        }
+
+        public bool HasPoint
+        {
+            get
+            {
+                return Position.HasValue;
+            }
+        }
+
+        public Point? Position
+        {
+            get
+            {
+                if (_point != null)
+                {
+                    return _point;
+                }
+
+                var point = _answerMetaDataItems.FirstOrDefault(m => m.Type == MetaDataType.Point2D)?.Value ?? string.Empty;
+                if (string.IsNullOrWhiteSpace(point))
+                {
+                    return null;
+                }
+
+                var positions = point.Split(";");
+                if (positions.Length != 2)
+                {
+                    return null;
+                }
+
+                if (!double.TryParse(positions[0], out double x) ||
+                    !double.TryParse(positions[1], out double y) ||
+                    double.IsInfinity(x) ||
+                    double.IsNaN(x) ||
+                    double.IsInfinity(y) ||
+                    double.IsNaN(y))
+                {
+                    return null;
+                }
+
+                _point = new Point(x, y);
+                return _point;
+            }
         }
 
         public static AnswerEntry Import(AnswerItem answerItem, IEnumerable<MetaDataItem> answerMetaDataItems)
