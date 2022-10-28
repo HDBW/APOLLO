de57597e0ee4c2f454bdc54d235257ae8f941152
Lars Heinrich <lh@heires.net>
2022-10-28 20:07:33 +0200

    Assessment Rendering WIP Fixed data issues in test

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Contracts/IInteractiveEntry.cs
@@ -0,0 +1,13 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+using Invite.Apollo.App.Graph.Common.Models.Assessment.Enums;
+
+namespace De.HDBW.Apollo.Client.Contracts
+{
+    public interface IInteractiveEntry
+    {
+        InteractionType Interaction { get; }
+
+        object Data { get; }
+    }
+}
