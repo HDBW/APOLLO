ae075a19f38fd8e147f1267f063dc46087594d62
Lars Heinrich <lh@heires.net>
2022-11-01 19:30:02 +0100

    fixed exception because of missing static resource during startup Added inital skinning for extended splash screen Registration and SplashScreen

--- src/client/Maui/UI/De.HDBW.Apollo.Client/App.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/App.xaml.cs
@@ -1,33 +1,42 @@
 ﻿// (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using Android.Media;
+using Android.Window;
+using De.HDBW.Apollo.Client.Models;
 using De.HDBW.Apollo.Client.Views;
+using De.HDBW.Apollo.Data.Services;
 using De.HDBW.Apollo.SharedContracts.Enums;
 using De.HDBW.Apollo.SharedContracts.Services;
+using Java.Security;
 
 namespace De.HDBW.Apollo.Client;
 public partial class App : Application
 {
+    private IServiceProvider _serviceProvider;
+    private ISessionService _sessionService;
+    private IPreferenceService _preferenceService;
+
     public App(
-        ExtendedSplashScreenView splashScreenView,
-        RegistrationView registrationView,
-        UseCaseTutorialView useCaseTutorialView,
+        IServiceProvider provider,
         IPreferenceService preferenceService,
         ISessionService sessionService)
     {
+        _serviceProvider = provider;
+        _sessionService = sessionService;
+        _preferenceService = preferenceService;
         InitializeComponent();
-
-        if (preferenceService.GetValue(Preference.IsFirstTime, false))
+        if (_preferenceService.GetValue(Preference.IsFirstTime, false))
         {
-            MainPage = new NavigationPage(splashScreenView);
+            MainPage = new NavigationPage(Routing.GetOrCreateContent(Routes.ExtendedSplashScreenView, _serviceProvider) as Page);
         }
-        else if (!sessionService.HasRegisteredUser)
+        else if (!_sessionService.HasRegisteredUser)
         {
-            MainPage = new NavigationPage(registrationView);
+            MainPage = new NavigationPage(Routing.GetOrCreateContent(Routes.RegistrationView, _serviceProvider) as Page);
         }
         else
         {
-            MainPage = new NavigationPage(useCaseTutorialView);
+            MainPage = new NavigationPage(Routing.GetOrCreateContent(Routes.UseCaseTutorialView, _serviceProvider) as Page);
         }
     }
 }
