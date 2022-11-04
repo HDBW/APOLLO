00f2f0622544690a5b3985cfded51aca63e62870
Larhei <lh@heires.net>
2022-11-04 12:44:54 +0100

    Layout issues on ios
    ios Wip

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Converter/InteractionsToMaximumHeightConverter.cs
@@ -0,0 +1,36 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using System.Globalization;
+using De.HDBW.Apollo.Client.Models.Interactions;
+using Invite.Apollo.App.Graph.Common.Models.Assessment;
+using Invite.Apollo.App.Graph.Common.Models.Course;
+
+namespace De.HDBW.Apollo.Client.Converter
+{
+    public class InteractionsToMaximumHeightConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || value == BindableProperty.UnsetValue)
+            {
+                return 0d;
+            }
+
+            if (!double.TryParse(parameter?.ToString(), out double rowHeight))
+            {
+                return 0d;
+            }
+
+            var items = value as IEnumerable<InteractionEntry> ?? new List<InteractionEntry>();
+            var rows = (double)items.Count() / 2d;
+            int rowCount = (int)Math.Ceiling(rows);
+            return (double)(rowCount * rowHeight);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"Twoway Binding not supported in {GetType()}.");
+        }
+    }
+}
