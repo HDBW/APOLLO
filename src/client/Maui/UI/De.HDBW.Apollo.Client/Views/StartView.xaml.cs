128219b62369aac9dc7efe0c9dcf45cb43354d6e
Lars Heinrich <lh@heires.net>
2022-11-04 14:42:03 +0100

    make it work on android

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/StartView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/StartView.xaml.cs
@@ -3,7 +3,9 @@
 
 using System.Globalization;
 using De.HDBW.Apollo.Client.Converter;
+using De.HDBW.Apollo.Client.Models.Interactions;
 using De.HDBW.Apollo.Client.ViewModels;
+using Kotlin.Time;
 
 namespace De.HDBW.Apollo.Client.Views;
 public partial class StartView
@@ -30,14 +32,21 @@ public partial class StartView
             return;
         }
 
+        var model = view.BindingContext as InteractionCategoryEntry;
+        if (model == null)
+        {
+            return;
+        }
+
         var converter = new InteractionsToMaximumHeightConverter();
-        var value = converter.Convert(view.ItemsSource, typeof(double), 240d, CultureInfo.CurrentUICulture);
+        var value = converter.Convert(model.Interactions, typeof(double), 240d, CultureInfo.CurrentUICulture);
         var doubleValue = 0d;
         if (value is double)
         {
             doubleValue = (double)value;
         }
 
-        view.MaximumHeightRequest = doubleValue;
+        view.HeightRequest = doubleValue;
+        view.ItemsSource = model.Interactions;
     }
 }
