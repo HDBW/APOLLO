d3e6c97a4edb65c12307a2986a2a09b18f9e3aa5
Lars Heinrich <lh@heires.net>
2022-11-07 21:50:41 +0100

    Assessment interactions select and multiselect

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Helper/Assessment/AnswerItemInteractionFactory.cs
@@ -0,0 +1,26 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using De.HDBW.Apollo.Client.Contracts;
+using De.HDBW.Apollo.Client.Models.Assessment;
+using Invite.Apollo.App.Graph.Common.Models.Assessment.Enums;
+using Microsoft.Extensions.Logging;
+
+namespace De.HDBW.Apollo.Client.Helper.Assessment
+{
+    public static class AnswerItemInteractionFactory
+    {
+        public static IAnswerItemInteraction Create(InteractionType interaction, QuestionEntry entry, ILogger logger)
+        {
+            switch (interaction)
+            {
+                case InteractionType.SingleSelect:
+                    return new SingleSelectInteraction(entry, logger);
+                case InteractionType.MultiSelect:
+                    return new MultiSelectSelectInteraction(entry, logger);
+                default:
+                    return null;
+            }
+        }
+    }
+}
