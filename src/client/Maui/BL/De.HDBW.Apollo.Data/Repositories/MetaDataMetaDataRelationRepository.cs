91e6dcc7c78e44772551797e4cdb41bbde4bdffc
Lars Heinrich <lh@heires.net>
2022-11-09 19:42:23 +0100

    wip

--- src/client/Maui/BL/De.HDBW.Apollo.Data/Repositories/MetaDataMetaDataRelationRepository.cs
+++ src/client/Maui/BL/De.HDBW.Apollo.Data/Repositories/MetaDataMetaDataRelationRepository.cs
@@ -1,6 +1,7 @@
 ﻿// (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using System.Collections.ObjectModel;
 using De.HDBW.Apollo.SharedContracts.Repositories;
 using Invite.Apollo.App.Graph.Common.Models.Assessment;
 using Microsoft.Extensions.Logging;
@@ -14,6 +15,18 @@ namespace De.HDBW.Apollo.Data.Repositories
         public MetaDataMetaDataRelationRepository(ILogger<MetaDataMetaDataRelationRepository> logger)
             : base(logger)
         {
+        }
+
+        public Task<IEnumerable<MetaDataMetaDataRelation>> GetItemsBySourceIdsAsync(IEnumerable<long> ids, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            var result = new ReadOnlyCollection<MetaDataMetaDataRelation>(new List<MetaDataMetaDataRelation>());
+            if (ids != null)
+            {
+                result = new ReadOnlyCollection<MetaDataMetaDataRelation>(Items.Where(i => ids.Contains(i.SourceId)).ToList());
+            }
+
+            return Task.FromResult(result as IEnumerable<MetaDataMetaDataRelation>);
         }
     }
 }
