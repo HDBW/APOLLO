23b3dbec0de6fb5305fe9a069f85d2c8226f0d1a
Patric Boscolo <patric@imenja.io>
2022-11-10 22:52:41 +0100

    added some backend stuff to dev

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Models/Asset.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Invite.Apollo.App.Graph.Common.Models;
+
+namespace Invite.Apollo.App.Graph.Assessment.Models
+{
+    public class Asset : IBackendEntity
+    {
+        #region Implementation of IBackendEntity
+
+        [Key]
+        public long BackendId { get; set; }
+
+        /// <summary>
+        /// Another Unique Identifier used as Uri for Services
+        /// </summary>
+        [Index(IsUnique = true)]
+        [Required]
+        [MaxLength(62)]
+        public Uri Schema { get; set; }
+
+        #endregion
+
+        public string ExternalId { get; set; }
+
+        public string assetName { get; set; }
+
+        public List<Uri> file { get; set; }
+
+        public List<Uri> Blob { get; set; }
+
+        public List<Uri> Cdn { get; set; }
+    }
+}
