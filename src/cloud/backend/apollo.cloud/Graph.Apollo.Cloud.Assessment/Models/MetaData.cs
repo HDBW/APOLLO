5c77c6674992149deacc9492633b3476b7ee3dbe
Patric Boscolo <patric@imenja.io>
2022-11-11 01:39:18 +0100

    Added Models to Assessment Service

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Models/MetaData.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Invite.Apollo.App.Graph.Common.Models.Assessment.Enums;
+
+namespace Invite.Apollo.App.Graph.Assessment.Models
+{
+    public class MetaData
+    {
+        #region Implementation of IBackendEntity
+
+        /// <summary>
+        /// The Id of the Backend Unique Identifier
+        /// </summary>
+        [Key]
+        public long BackendId { get; set; }
+
+        /// <summary>
+        /// Another Unique Identifier used as Uri for Services
+        /// </summary>
+        [Index(IsUnique = true)]
+        [Required]
+        [MaxLength(62)]
+        public Uri Schema { get; set; } = new Uri($"https://invite-apollo.app/{Guid.NewGuid()}");
+
+        #endregion
+
+        public MetaDataType Type { get; set; }
+
+        public string Value { get; set; }
+
+        public Asset Asset { get; set; }
+
+        public MetaData MetaDataHasMetaData { get; set; }
+    }
+}
