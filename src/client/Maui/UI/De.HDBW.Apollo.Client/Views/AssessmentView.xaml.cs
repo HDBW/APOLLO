2f698cd4370d8d88d438428bebd772c28f7d8357
Larhei <lh@heires.net>
2022-11-11 15:21:02 +0100

    Fixed assessment rendering on ios

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/AssessmentView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/AssessmentView.xaml.cs
@@ -1,6 +1,7 @@
 // (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using System;
 using De.HDBW.Apollo.Client.ViewModels;
 
 namespace De.HDBW.Apollo.Client.Views;
@@ -19,5 +20,23 @@ public partial class AssessmentView
         {
             return BindingContext as AssessmentViewModel;
         }
+    }
+
+    private void HandleChildAdded(object sender, ElementEventArgs e)
+    {
+        var layout = sender as Layout;
+        if (layout == null)
+        {
+            return;
+        }
+
+        layout.MinimumHeightRequest = 0;
+        var size = layout.Measure(double.PositiveInfinity, double.PositiveInfinity, MeasureFlags.None);
+        if (double.IsNaN(size.Request.Height) || double.IsInfinity(size.Request.Height))
+        {
+            return;
+        }
+
+        layout.MinimumHeightRequest = Math.Max(size.Request.Height, size.Minimum.Height);
     }
 }
