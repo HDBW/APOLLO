2fa8d7798d4819f026bc096c4502f71438dff5f8
Patric Boscolo <patric@imenja.io>
2022-11-11 03:11:32 +0100

    added sql connection

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Data/AssessmentContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Invite.Apollo.App.Graph.Assessment.Data
+{
+    public class AssessmentContext : DbContext
+    {
+        public AssessmentContext(DbContextOptions<AssessmentContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Models.Assessment> Assessments { get; set; }
+        public DbSet<Models.Answer> Answers { get; set; }
+        public DbSet<Models.Asset> Assets { get; set; }
+        public DbSet<Models.MetaData> MetaDatas { get; set; }
+        public DbSet<Models.AssessmentCategory> AssessmentCategories { get; set; }
+        public DbSet<Models.Question> Questions { get; set; }
+        public DbSet<Models.Scores> Scores { get; set; }
+
+    }
+}
