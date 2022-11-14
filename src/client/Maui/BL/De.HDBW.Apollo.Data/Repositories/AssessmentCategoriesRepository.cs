ed0febe3fd07c52807a3d4f93bea103564e67d89
Patric Boscolo <patric@imenja.io>
2022-11-15 00:17:26 +0100

    Added Assessment Repository and Started Result Service

---
+++ src/client/Maui/BL/De.HDBW.Apollo.Data/Repositories/AssessmentCategoriesRepository.cs
@@ -0,0 +1,25 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using System.Collections.ObjectModel;
+using De.HDBW.Apollo.SharedContracts.Repositories;
+using Invite.Apollo.App.Graph.Common.Models.Assessment;
+using Microsoft.Extensions.Logging;
+
+namespace De.HDBW.Apollo.Data.Repositories
+{
+    public class AssessmentCategoriesRepository : AbstractInMemoryRepository<AssessmentCategory>,
+        IAssessmentCategoriesRepository
+    {
+        public AssessmentCategoriesRepository(ILogger logger)
+            : base(logger)
+        {
+        }
+
+        public Task<List<AssessmentCategory>> GetItemByForeignKeyAsync(long id, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            return Task.FromResult(new List<AssessmentCategory>());
+        }
+    }
+}
