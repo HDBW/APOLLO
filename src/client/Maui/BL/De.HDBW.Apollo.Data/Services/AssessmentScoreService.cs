3506e02ccec5f2ec4659269ba04f40e61aee037d
Patric Boscolo <patric@imenja.io>
2022-11-16 10:31:12 +0100

    Merged with #55 origin
    Co-Authored-By: talisi <5801746+talisi@users.noreply.github.com>
    Co-Authored-By: Larhei <47631685+Larhei@users.noreply.github.com>

--- src/client/Maui/BL/De.HDBW.Apollo.Data/Services/AssessmentScoreService.cs
+++ src/client/Maui/BL/De.HDBW.Apollo.Data/Services/AssessmentScoreService.cs
@@ -3,6 +3,7 @@
 
 using De.HDBW.Apollo.SharedContracts.Repositories;
 using De.HDBW.Apollo.SharedContracts.Services;
+using Invite.Apollo.App.Graph.Common.Models.Assessment;
 using Invite.Apollo.App.Graph.Common.Models.UserProfile;
 using Microsoft.Extensions.Logging;
 
@@ -10,40 +11,66 @@ namespace De.HDBW.Apollo.Data.Services
 {
     public class AssessmentScoreService : IAssessmentScoreService
     {
+        private readonly IQuestionItemRepository _questions;
+        private readonly IAnswerItemRepository _answers;
+        private readonly IAssessmentCategoryRepository _categories;
+        private readonly ILogger<AssessmentScoreService> _logger;
+
         public AssessmentScoreService(
             ILogger<AssessmentScoreService>? logger,
-            IQuestionItemRepository? questionItemRepository,
-            IAnswerItemRepository? answerItemRepository,
-            IAssessmentCategoryRepository? assessmentCategoriesRepository)
+            IQuestionItemRepository questions,
+            IAnswerItemRepository answers,
+            IAssessmentCategoryRepository categoriesRepository)
         {
-            Logger = logger;
-            QuestionItemRepository = questionItemRepository;
-            AnswerItemRepository = answerItemRepository;
-            AssessmentCategoriesRepository = assessmentCategoriesRepository;
+            _logger = logger;
+            _questions = questions;
+            _answers = answers;
+            _categories = categoriesRepository;
         }
 
-        private IQuestionItemRepository? QuestionItemRepository { get; }
-
-        private IAnswerItemRepository? AnswerItemRepository { get; }
-
-        private IAssessmentCategoryRepository? AssessmentCategoriesRepository { get; }
-
-        private ILogger? Logger { get; }
-
-        public Task<AssessmentScore> GetAssessmentScoreAsync(IEnumerable<AnswerItemResult> answerItemResults, CancellationToken token)
+        public async Task<AssessmentScore> GetAssessmentScoreAsync(IEnumerable<AnswerItemResult> answerItemResults, CancellationToken token)
         {
+            _logger.Log(LogLevel.Information, new EventId(101, "GetAssessmentScore Called"), "{answerItems.Dump()}", answerItemResults);
+
+            AssessmentScore score = new();
+            List<AssessmentCategoryResult> results = new List<AssessmentCategoryResult>();
+
             token.ThrowIfCancellationRequested();
-            Logger?.Log(LogLevel.Information, new EventId(101, "GetAssessmentScore Called"), "{answerItems.Dump()}", answerItemResults);
 
-            AssessmentScore score = new ();
+            //retrive assessmentId
+            long assessmentId = answerItemResults.FirstOrDefault()!.AssessmentItemId;
 
-            // TODO: Iterate over answerItems
-            // TODO: Load Assessment CategoryResult
-            // TODO: Create CategoryResults Collection
+            // TODO: Iterate over answerItems and create Category result
+            var questions = await _questions.GetItemsByForeignKeyAsync(assessmentId, token);
+
+            //iterate through the questions and check if there are answeritems for the questions
+            foreach (QuestionItem question in questions)
+            {
+                //load question specific information just as answers and results
+                var questionCategory = await _categories.GetItemByIdAsync(question.Category, token);
+                List<long> questionIds = new() { question.Id };
+                var answers = await _answers.GetItemsByForeignKeysAsync(questionIds, token);
+
+                //check if we have a answerItemResult for a question
+                List<AnswerItemResult> usersResultsForQuestion =
+                    answerItemResults.Where(a => a.QuestionItemId == question.Id).ToList();
+                if (usersResultsForQuestion.Count > 0)
+                {
+                    //TODO: score based on vector x scalar?
+                }
+                else
+                {
+                    results.Add(new AssessmentCategoryResult
+                    {
+                        Category = questionCategory.Id,
+                        Result = 0,
+                        //UserProfileId = TODO: Need User ProfileId here ^_^
+                    });
+                }
+            }
             AssessmentCategoryResult categoryResult = new AssessmentCategoryResult();
-
-            // TODO: Calculate Assessment Score and be happy ^_^
-            return Task.FromResult(score);
+            // TODO: Calculate Assessment Score based on results and store that shit and be happy ^_^
+            return score;
         }
     }
 }
