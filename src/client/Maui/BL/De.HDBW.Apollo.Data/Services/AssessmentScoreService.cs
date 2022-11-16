fd83cf0b00354d79d92816ad99b1a001efcad799
Larhei <lh@heires.net>
2022-11-16 11:29:13 +0100

    Cleanup, added userid

--- src/client/Maui/BL/De.HDBW.Apollo.Data/Services/AssessmentScoreService.cs
+++ src/client/Maui/BL/De.HDBW.Apollo.Data/Services/AssessmentScoreService.cs
@@ -1,6 +1,7 @@
 ﻿// (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using De.HDBW.Apollo.Data.Repositories;
 using De.HDBW.Apollo.SharedContracts.Repositories;
 using De.HDBW.Apollo.SharedContracts.Services;
 using Invite.Apollo.App.Graph.Common.Models.Assessment;
@@ -11,64 +12,80 @@ namespace De.HDBW.Apollo.Data.Services
 {
     public class AssessmentScoreService : IAssessmentScoreService
     {
-        private readonly IQuestionItemRepository _questions;
-        private readonly IAnswerItemRepository _answers;
-        private readonly IAssessmentCategoryRepository _categories;
-        private readonly ILogger<AssessmentScoreService> _logger;
-
         public AssessmentScoreService(
             ILogger<AssessmentScoreService>? logger,
-            IQuestionItemRepository questions,
-            IAnswerItemRepository answers,
-            IAssessmentCategoryRepository categoriesRepository)
+            IUserProfileItemRepository userProfileItemRepository,
+            IQuestionItemRepository questionItemRepository,
+            IAnswerItemRepository answerItemRepository,
+            IAssessmentCategoryRepository assessmentCategoriesRepository)
         {
-            _logger = logger;
-            _questions = questions;
-            _answers = answers;
-            _categories = categoriesRepository;
+            ArgumentNullException.ThrowIfNull(userProfileItemRepository);
+            ArgumentNullException.ThrowIfNull(questionItemRepository);
+            ArgumentNullException.ThrowIfNull(answerItemRepository);
+            ArgumentNullException.ThrowIfNull(assessmentCategoriesRepository);
+            Logger = logger;
+            UserProfileItemRepository = userProfileItemRepository;
+            QuestionItemRepository = questionItemRepository;
+            AnswerItemRepository = answerItemRepository;
+            AssessmentCategoriesRepository = assessmentCategoriesRepository;
         }
+
+        private ILogger? Logger { get; }
+
+        private IUserProfileItemRepository UserProfileItemRepository { get; }
+
+        private IQuestionItemRepository QuestionItemRepository { get; }
+
+        private IAnswerItemRepository AnswerItemRepository { get; }
+
+        private IAssessmentCategoryRepository AssessmentCategoriesRepository { get; }
 
         public async Task<AssessmentScore> GetAssessmentScoreAsync(IEnumerable<AnswerItemResult> answerItemResults, CancellationToken token)
         {
-            _logger.Log(LogLevel.Information, new EventId(101, "GetAssessmentScore Called"), "{answerItems.Dump()}", answerItemResults);
+            Logger?.Log(LogLevel.Information, new EventId(101, "GetAssessmentScore Called"), "{answerItems.Dump()}", answerItemResults);
 
-            AssessmentScore score = new();
+            AssessmentScore score = new ();
             List<AssessmentCategoryResult> results = new List<AssessmentCategoryResult>();
 
             token.ThrowIfCancellationRequested();
 
-            //retrive assessmentId
+            // retrive assessmentId
             long assessmentId = answerItemResults.FirstOrDefault()!.AssessmentItemId;
 
             // TODO: Iterate over answerItems and create Category result
-            var questions = await _questions.GetItemsByForeignKeyAsync(assessmentId, token);
+            var questions = await QuestionItemRepository.GetItemsByForeignKeyAsync(assessmentId, token).ConfigureAwait(false);
 
-            //iterate through the questions and check if there are answeritems for the questions
+            var userProfiles = await UserProfileItemRepository.GetItemsAsync(token).ConfigureAwait(false);
+            var userId = userProfiles.First().Id;
+
+            // iterate through the questions and check if there are answeritems for the questions
             foreach (QuestionItem question in questions)
             {
-                //load question specific information just as answers and results
-                var questionCategory = await _categories.GetItemByIdAsync(question.Category, token);
-                List<long> questionIds = new() { question.Id };
-                var answers = await _answers.GetItemsByForeignKeysAsync(questionIds, token);
+                // load question specific information just as answers and results
+                var questionCategory = await AssessmentCategoriesRepository.GetItemByIdAsync(question.Category, token).ConfigureAwait(false);
+                List<long> questionIds = new () { question.Id };
+                var answers = await AnswerItemRepository.GetItemsByForeignKeysAsync(questionIds, token).ConfigureAwait(false);
 
-                //check if we have a answerItemResult for a question
+                // check if we have a answerItemResult for a question
                 List<AnswerItemResult> usersResultsForQuestion =
                     answerItemResults.Where(a => a.QuestionItemId == question.Id).ToList();
                 if (usersResultsForQuestion.Count > 0)
                 {
-                    //TODO: score based on vector x scalar?
+                    // TODO: score based on vector x scalar?
                 }
                 else
                 {
                     results.Add(new AssessmentCategoryResult
                     {
-                        Category = questionCategory.Id,
+                        Category = questionCategory?.Id ?? -1,
                         Result = 0,
-                        //UserProfileId = TODO: Need User ProfileId here ^_^
+                        UserProfileId = userId,
                     });
                 }
             }
+
             AssessmentCategoryResult categoryResult = new AssessmentCategoryResult();
+
             // TODO: Calculate Assessment Score based on results and store that shit and be happy ^_^
             return score;
         }
