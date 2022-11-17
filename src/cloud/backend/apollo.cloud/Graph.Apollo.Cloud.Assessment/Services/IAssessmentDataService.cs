b0b259bdb0ed0c17008a1b1e382c5c50f92f3c45
Patric Boscolo <patric@imenja.io>
2022-11-17 11:05:27 +0100

    Add: AssessmentDataService Add: ToDo comments

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Services/IAssessmentDataService.cs
@@ -0,0 +1,20 @@
+﻿namespace Invite.Apollo.App.Graph.Assessment.Services
+{
+    public interface IAssessmentDataService
+    {
+
+        public Task<IEnumerable<Models.Assessment>> GetAllAssessmentsAsync();
+
+        public Task<IEnumerable<Models.Assessment>> GetAssessmentByIdAsync(long assessmentId);
+
+        public Task<IEnumerable<Models.Assessment>> GetAssessmentByOccupation(string occupation);
+
+        public Task<IQueryable<Models.Assessment>> FindAllAssessmentsAsync();
+
+        public void CreateAssessment(Models.Assessment assessment);
+
+        public void EditAssessmentAsync(Models.Assessment assessment);
+
+        public void DeleteAssessmentAsync(Models.Assessment assessment);
+    }
+}
