fe00d3271cb6d8bd57429edfdcfe9a5b6db1e710
Patric Boscolo <patric@imenja.io>
2022-11-22 17:50:02 +0100

    added scalar to question added scalar to questionitem

--- src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Services/AssessmentService.cs
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Services/AssessmentService.cs
@@ -1,4 +1,8 @@
+using System.Collections.ObjectModel;
+using Invite.Apollo.App.Graph.Common.Models;
 using Invite.Apollo.App.Graph.Common.Models.Assessment;
+using Newtonsoft.Json;
+using ProtoBuf;
 using ProtoBuf.Grpc;
 
 namespace Invite.Apollo.App.Graph.Assessment.Services;
@@ -8,13 +12,44 @@ public class AssessmentService : IAssessmentService
     private readonly ILogger<AssessmentService> _logger;
     private readonly IDataService _assessmentDataService;
 
-    //TODO: Implement Collections
+    private UseCaseCollections _collections;
 
 
     public AssessmentService(ILogger<AssessmentService> logger, IDataService assessmentDataService)
     {
         _logger = logger;
         _assessmentDataService = assessmentDataService;
+        _collections = new UseCaseCollections
+        {
+            AssessmentCategories = new Collection<AssessmentCategory>(_assessmentDataService.GetAllAssessmentCategoriesAsync().Result.ToList()),
+            AssessmentItems = new Collection<AssessmentItem>(_assessmentDataService.GetAllAssessmentItemsAsync().Result.ToList()),
+            AnswerItems = new Collection<AnswerItem>(_assessmentDataService.GetAllAnswerItemsAsync().Result.ToList()),
+            AnswerMetaDataRelations = new Collection<AnswerMetaDataRelation>(_assessmentDataService.GetAllAnswerMetaDataRelationsAsync().Result.ToList()),
+            MetaDataItems = new Collection<MetaDataItem>(_assessmentDataService.GetAllMetaDataItemsAsync().Result.ToList()),
+            MetaDataMetaDataRelations = new Collection<MetaDataMetaDataRelation>(_assessmentDataService.GetAllMetaDataMetaDataRelationsAsync().Result.ToList()),
+            QuestionItems = new Collection<QuestionItem>(_assessmentDataService.GetAllQuestionItemsAsync().Result.ToList()),
+            QuestionMetaDataRelations = new Collection<QuestionMetaDataRelation>(_assessmentDataService.GetAllQuestionMetaDataRelationsAsync().Result.ToList()),
+            //TODO: Courses
+        };
+
+        using (StreamWriter file = File.CreateText(@"devtest.json"))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(file, _collections);
+        }
+
+        string filename = "usecase1.bin";
+
+        if (File.Exists(filename))
+        {
+            File.Delete(filename);
+        }
+
+        using (var file = File.Create(filename))
+        {
+            Serializer.Serialize(file, _collections);
+            file.Close();
+        }
     }
 
 
