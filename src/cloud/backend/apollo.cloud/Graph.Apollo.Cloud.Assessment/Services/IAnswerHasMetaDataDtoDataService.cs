2ab61e2564b8dd19e07a30284292bcee4d5004f4
Patric Boscolo <patric@imenja.io>
2022-11-22 03:40:42 +0100

    Implemented DataService Interfaces for DTO

---
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Services/IAnswerHasMetaDataDtoDataService.cs
@@ -0,0 +1,17 @@
+﻿using Invite.Apollo.App.Graph.Common.Models.Assessment;
+
+namespace Invite.Apollo.App.Graph.Assessment.Services
+{
+    public interface IAnswerHasMetaDataDtoDataService
+    {
+        public Task<IEnumerable<AnswerMetaDataRelation>> GetAllAnswerMetaDataRelationsAsync();
+
+        public Task<IEnumerable<AnswerMetaDataRelation>> GetAnswerMetaDataRelationByAnswerIdAsync(long answerId);
+
+        public void CreateAnswerMetaDataRelation(AnswerMetaDataRelation answerMetaDataRelation);
+
+        public void EditAnswerMetaDataRelation(AnswerMetaDataRelation answerMetaDataRelation);
+
+        public void DeleteAnswerMetaDataRelation(AnswerMetaDataRelation answerMetaDataRelation);
+    }
+}
