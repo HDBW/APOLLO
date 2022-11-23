a2eb80631f0e0628065e2503ac8e912bf8583638
Patric Boscolo <patric@imenja.io>
2022-11-23 14:40:26 +0100

    Updated Courses to meet schema
    Co-Authored-By: talisi <5801746+talisi@users.noreply.github.com>

--- src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Models/Course/Contact.cs
+++ src/cloud/backend/apollo.cloud/Graph.Apollo.Cloud.Assessment/Models/Course/Contact.cs
@@ -11,7 +11,7 @@ namespace Invite.Apollo.App.Graph.Assessment.Models.Course
         public string ContactPhone { get; set; } = string.Empty;
         public Uri Url { get; set; } = null!;
 
-        public Uri Image { get; set; } = null!;
+        //public Uri Image { get; set; } = null!;
 
         public List<CourseHasContacts> CourseContacts { get; set; }
     }
