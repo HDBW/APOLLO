e388fcd5698bb034828b9733dcedf1db3a274688
Lars Heinrich <lh@heires.net>
2022-11-25 19:58:49 +0100

    CourseView Wip

--- src/client/Maui/BL/De.HDBW.Apollo.Data/Repositories/CourseAppointmentRepository.cs
+++ src/client/Maui/BL/De.HDBW.Apollo.Data/Repositories/CourseAppointmentRepository.cs
@@ -1,9 +1,12 @@
 ﻿// (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using System.Collections.ObjectModel;
 using De.HDBW.Apollo.SharedContracts.Repositories;
+using Invite.Apollo.App.Graph.Common.Models.Assessment;
 using Invite.Apollo.App.Graph.Common.Models.Course;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace De.HDBW.Apollo.Data.Repositories
 {
@@ -14,6 +17,13 @@ namespace De.HDBW.Apollo.Data.Repositories
         public CourseAppointmentRepository(ILogger<CourseAppointmentRepository> logger)
             : base(logger)
         {
+        }
+
+        public Task<IEnumerable<CourseAppointment>> GetItemsByForeignKeyAsync(long id, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            var result = new ReadOnlyCollection<CourseAppointment>(Items.Where(i => i.CourseId == id).ToList());
+            return Task.FromResult(result as IEnumerable<CourseAppointment>);
         }
     }
 }
