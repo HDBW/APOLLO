c840245214bc42ed321747fe12cc20dff7e9f91b
Larhei <lh@heires.net>
2022-11-26 20:01:12 +0100

    CourseView Wip

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/CourseView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/CourseView.xaml.cs
@@ -17,4 +17,35 @@ public partial class CourseView
             return BindingContext as CourseViewModel;
         }
     }
+
+    private void HandleChildAdded(object sender, ElementEventArgs e)
+    {
+#if IOS
+        UpdateMinHeight(sender as Layout);
+#endif
+    }
+
+    private void HandleSizeChanged(object sender, EventArgs e)
+    {
+#if IOS
+        UpdateMinHeight(sender as Layout);
+#endif
+    }
+
+    private void UpdateMinHeight(Layout? layout)
+    {
+        if (layout == null)
+        {
+            return;
+        }
+
+        layout.MinimumHeightRequest = 0;
+        var size = layout.Measure(double.PositiveInfinity, double.PositiveInfinity, MeasureFlags.None);
+        if (double.IsNaN(size.Request.Height) || double.IsInfinity(size.Request.Height))
+        {
+            return;
+        }
+
+        layout.MinimumHeightRequest = Math.Max(size.Request.Height, size.Minimum.Height);
+    }
 }
