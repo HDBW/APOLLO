e985c10bd114b9e7d1fd98cf31c043fae4922810
Patric Boscolo <patric@imenja.io>
2022-11-29 15:38:28 +0100

    Working on AssessmentResult

---
+++ src/client/Maui/BL/De.HDBW.Apollo.Data/Repositories/AssessmentCategoryResultRepository.cs
@@ -0,0 +1,34 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using De.HDBW.Apollo.SharedContracts.Repositories;
+using Invite.Apollo.App.Graph.Common.Models.Assessment;
+using Invite.Apollo.App.Graph.Common.Models.UserProfile;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace De.HDBW.Apollo.Data.Repositories
+{
+    public class AssessmentCategoryResultRepository :
+        AbstractInMemoryRepository<AssessmentCategoryResult>,
+        IAssessmentCategoryResultRepository
+    {
+        public AssessmentCategoryResultRepository(ILogger<QuestionItemRepository> logger)
+            : base(logger)
+        {
+        }
+
+        public Task<IEnumerable<AssessmentCategoryResult>> GetItemsByForeignKeyAsync(long id, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            return Task.FromResult(new ReadOnlyCollection<AssessmentCategoryResult>(Items?.Where(i => i.Category == id).ToList() ?? new List<AssessmentCategoryResult>()) as IEnumerable<AssessmentCategoryResult>);
+        }
+    }
+}
---
+++ src/client/Maui/Shared/De.HDBW.Apollo.SharedContracts/Repositories/IAssessmentCategoryResultRepository.cs
@@ -0,0 +1,25 @@
+﻿#region (c) Licensed to the HDBW under one or more agreements.\nThe HDBW licenses this file to you under the MIT license.
+
+// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+#endregion
+
+using De.HDBW.Apollo.SharedContracts.Repositories;
+using Invite.Apollo.App.Graph.Common.Models.UserProfile;
+
+namespace De.HDBW.Apollo.SharedContracts.Repositories
+{
+    public interface IAssessmentCategoryResultRepository :
+        IRepository<AssessmentCategoryResult>,
+        IDatabaseRepository<AssessmentCategoryResult>
+    {
+        /// <summary>
+        /// Returns all AssessmentCategoryResults for a specific Question Category.
+        /// </summary>
+        /// <param name="id">QuestionCategory</param>
+        /// <param name="token">Cancellation Token</param>
+        /// <returns>List of AssessmentCategoryResults</returns>
+        Task<IEnumerable<AssessmentCategoryResult>> GetItemsByForeignKeyAsync(long id, CancellationToken token);
+    }
+}
