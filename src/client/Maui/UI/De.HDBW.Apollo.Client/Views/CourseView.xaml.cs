bc2f890675dc9ba39efaf0ac2f47724d99e7e0d3
Lars Heinrich <lh@heires.net>
2022-12-09 15:30:51 +0100

    memory cleanup on windows and ios

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/CourseView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/CourseView.xaml.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using De.HDBW.Apollo.Client.ViewModels;
 
 namespace De.HDBW.Apollo.Client.Views;
@@ -6,8 +7,18 @@ public partial class CourseView
 {
     public CourseView(CourseViewModel model)
     {
+#if DEBUG
+        Debug.WriteLine($"Create {GetType()}");
+#endif
         InitializeComponent();
         BindingContext = model;
+    }
+
+    ~CourseView()
+    {
+#if DEBUG
+        Debug.WriteLine($"~{GetType()}");
+#endif
     }
 
     public CourseViewModel? ViewModel
