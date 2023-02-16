3315be4232f81c4d04c41abcfa0259f589d12b08
Larhei <47631685+Larhei@users.noreply.github.com>
2023-02-16 16:45:33 +0100

    Removed iOS Workarround for Scrollviewers

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/StartView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/StartView.xaml.cs
@@ -76,25 +76,5 @@ public partial class StartView
         }
 
         await PART_ScrollHost.ScrollToAsync(0, 0, false);
-        HandleSizeChanged(PART_ScrollHost, null);
-    }
-
-    private void HandleSizeChanged(object sender, EventArgs? e)
-    {
-#if IOS
-        var layout = PART_ScrollHost?.Content;
-        if (layout == null)
-        {
-            return;
-        }
-
-        layout.MinimumHeightRequest = 0;
-        var size = layout.Measure(double.PositiveInfinity, double.PositiveInfinity, MeasureFlags.None);
-        layout.MinimumHeightRequest = Math.Max(size.Request.Height, size.Minimum.Height);
-
-        if (layout.MinimumHeightRequest != PART_ScrollHost?.ContentSize.Height)
-        {
-        }
-#endif
     }
 }
