9d0b21f0ea3c44bdc86f8bfdcbea63b8ab1ce562
Larhei <47631685+Larhei@users.noreply.github.com>
2023-02-16 16:45:33 +0100

    Removed iOS Workarround for Scrollviewers

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/AssessmentView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/AssessmentView.xaml.cs
@@ -68,24 +68,4 @@ public partial class AssessmentView
             base.OnNavigatingFrom(args);
         }
     }
-
-    private void HandleChildAdded(object sender, ElementEventArgs e)
-    {
-#if IOS
-        var layout = sender as Layout;
-        if (layout == null)
-        {
-            return;
-        }
-
-        layout.MinimumHeightRequest = 0;
-        var size = layout.Measure(double.PositiveInfinity, double.PositiveInfinity, MeasureFlags.None);
-        if (double.IsNaN(size.Request.Height) || double.IsInfinity(size.Request.Height))
-        {
-            return;
-        }
-
-        layout.MinimumHeightRequest = Math.Max(size.Request.Height, size.Minimum.Height);
-#endif
-    }
 }
