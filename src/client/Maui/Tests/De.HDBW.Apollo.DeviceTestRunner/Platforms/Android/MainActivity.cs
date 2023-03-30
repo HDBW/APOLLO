81b33e2721d586a92e4908019fe6f443644e684b
Lars Heinrich <lh@heires.net>
2023-03-30 18:42:14 +0200

    test

--- src/client/Maui/Tests/De.HDBW.Apollo.DeviceTestRunner/Platforms/Android/MainActivity.cs
+++ src/client/Maui/Tests/De.HDBW.Apollo.DeviceTestRunner/Platforms/Android/MainActivity.cs
@@ -9,5 +9,6 @@ namespace De.HDBW.Apollo.DeviceTestRunner
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+
     }
 }
