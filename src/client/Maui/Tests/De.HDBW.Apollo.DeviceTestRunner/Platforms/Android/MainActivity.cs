0ebdff75ba4cde0743712b79192febfb5519903e
Lars Heinrich <lh@heires.net>
2023-03-30 18:54:15 +0200

    fixed build pipelines

--- src/client/Maui/Tests/De.HDBW.Apollo.DeviceTestRunner/Platforms/Android/MainActivity.cs
+++ src/client/Maui/Tests/De.HDBW.Apollo.DeviceTestRunner/Platforms/Android/MainActivity.cs
@@ -9,6 +9,5 @@ namespace De.HDBW.Apollo.DeviceTestRunner
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-
     }
 }
