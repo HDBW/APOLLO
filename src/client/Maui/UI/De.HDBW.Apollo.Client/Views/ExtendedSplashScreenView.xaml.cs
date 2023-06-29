5cc82ed97e6d5f199d1e060aac80cb3f44c0f2b9
Larhei <47631685+Larhei@users.noreply.github.com>
2023-06-29 23:44:12 +0200

    Fixed wrong height calculation of CarouselView on iOS after .net upgrade

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/ExtendedSplashScreenView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/ExtendedSplashScreenView.xaml.cs
@@ -3,6 +3,8 @@
 
 using System.Diagnostics;
 using De.HDBW.Apollo.Client.ViewModels;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
 
 namespace De.HDBW.Apollo.Client.Views;
 public partial class ExtendedSplashScreenView
@@ -29,5 +31,37 @@ public partial class ExtendedSplashScreenView
         {
             return BindingContext as ExtendedSplashScreenViewModel;
         }
+    }
+
+    private void HandleSizeChanged(Object sender, System.EventArgs e)
+    {
+#if IOS
+        var view = sender as IView;
+        var grid = view as Grid;
+        if (grid == null)
+        {
+            grid = view?.Parent as Grid ?? null;
+        }
+
+        var height = grid?.Height ?? 0d;
+        var heightSum = 0d;
+        foreach (var child in grid.Children)
+        {
+            switch (child)
+            {
+                case CarouselView _:
+                    break;
+
+                default:
+                    var handler = child.Handler;
+                    var size = handler?.GetDesiredSize(grid.Width, grid.Height) ?? Size.Zero;
+                    heightSum += size.Height + child.Margin.Top + child.Margin.Bottom;
+                    break;
+            }
+        }
+
+        var diff = height - heightSum;
+        PART_Animation.MaximumHeightRequest = diff;
+#endif
     }
 }
