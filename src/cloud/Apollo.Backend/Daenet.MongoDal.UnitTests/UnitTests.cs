e77c6432324ad6650e4b5a20d99bf7304eda2034
Damir Dobric <ddobric@daenet.com>
2023-11-01 23:30:48 +0100

    work on unit tests

--- src/cloud/Apollo.Backend/Daenet.MongoDal.UnitTests/UnitTests.cs
+++ src/cloud/Apollo.Backend/Daenet.MongoDal.UnitTests/UnitTests.cs
@@ -1,6 +1,8 @@
 // (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using Daenet.MongoDal.Entitties;
+
 namespace Daenet.MongoDal.UnitTests
 {
     [TestClass]
@@ -12,8 +14,15 @@ namespace Daenet.MongoDal.UnitTests
         {
             var dal = Helpers.GetDal();
 
-            await dal.ExecuteQuery("users", null, new Entitties.Query()
+            var res = await dal.ExecuteQuery("users", null, new Entitties.Query()
             {
+                 Fields = new List<FieldExpression>()
+                 {
+                     new FieldExpression(){ FieldName = "Name",
+                         Operator = QueryOperator.Contains,
+                          Argument = new List<object>(){"test"}
+                      }
+                 }                  
 
             }, 100, 0);
         }
