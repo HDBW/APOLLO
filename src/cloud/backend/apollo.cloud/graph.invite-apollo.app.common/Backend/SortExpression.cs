e95f836641fc84fcaa9d69327857d1e8a0a517f2
Patric Boscolo <patric@imenja.io>
2023-12-11 16:07:12 +0100

    Updated Nuget for Shared.cs

---
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/SortExpression.cs
@@ -0,0 +1,49 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo.Common.Entities
+{
+    /// <summary>
+    /// Define the sorting operation over a certain field, order ascending: 1, descending: -1
+    /// </summary>
+    public class SortExpression
+    {
+        /// <summary>
+        /// Field name to be sort
+        /// </summary>
+        public string FieldName { get; set; }
+
+        /// <summary>
+        /// sort order
+        /// </summary>
+        public SortOrder Order { get; set; }
+
+        //WARNING: This is not working in Shared Projects
+        //public override string ToString()
+        //{
+        //    return $"Field={FieldName}-{Enum.GetName(Order)}";
+        //}
+    }
+
+    /// <summary>
+    /// Define the possible sort order
+    /// </summary>
+    public enum SortOrder
+    {
+        /// <summary>
+        /// Ascending order
+        /// </summary>
+        Ascending = 1,
+
+        /// <summary>
+        /// Descending order
+        /// </summary>
+        Descending = -1
+    }
+}
