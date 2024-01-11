b7ce194af7700a61466cdc15af82fcf442662395
Lars Heinrich <lh@heires.net>
2024-01-11 11:34:26 +0100

    wip

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Platforms/Windows/App.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Platforms/Windows/App.xaml.cs
@@ -1,21 +1,22 @@
 ﻿// (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
-namespace De.HDBW.Apollo.Client.WinUI;
-
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
-public partial class App : MauiWinUIApplication
+namespace De.HDBW.Apollo.Client.WinUI
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="App"/> class.
-    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    public App()
+    public partial class App : MauiWinUIApplication
     {
-        InitializeComponent();
-    }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
 
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
