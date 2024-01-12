93243458613fd297e917a05e0a0f04c142d74386
Mukit Khan <mkhan@daenet.com>
2024-01-12 01:16:52 +0100

    Add common input validator attiribute

---
+++ src/cloud/Apollo.Backend/Apollo.Service.Common/Attributes/ValidDateTimeAttribute.cs
@@ -0,0 +1,27 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo.Common.Attributes
+{
+   
+        [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = false)]
+        public class ValidDateTimeAttribute : ValidationAttribute
+        {
+            public override bool IsValid(object value)
+            {
+                if (value is DateTime)
+                {
+                return true;
+                }
+
+                return false; // Not a DateTime
+            }
+        }
+}
