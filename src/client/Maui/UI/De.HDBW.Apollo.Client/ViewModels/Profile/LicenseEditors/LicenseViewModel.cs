616f067b40cc2d753afd278dde65a247f97f415f
Lars Heinrich <lh@heires.net>
2024-01-23 16:09:13 +0100

    Improved LicenseViewModel

--- src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/LicenseEditors/LicenseViewModel.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/LicenseEditors/LicenseViewModel.cs
@@ -1,34 +1,49 @@
 ﻿// (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using System.ComponentModel.DataAnnotations;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using De.HDBW.Apollo.Client.Contracts;
+using De.HDBW.Apollo.Client.Helper;
+using De.HDBW.Apollo.Client.Models;
+using De.HDBW.Apollo.SharedContracts.Repositories;
+using De.HDBW.Apollo.SharedContracts.Services;
 using Invite.Apollo.App.Graph.Common.Models.UserProfile;
 using Microsoft.Extensions.Logging;
+using UserProfile = Invite.Apollo.App.Graph.Common.Models.UserProfile.Profile;
 
 namespace De.HDBW.Apollo.Client.ViewModels.Profile.LicenseEditors
 {
-    public partial class LicenseViewModel : BaseViewModel
+    public partial class LicenseViewModel : AbstractSaveDataViewModel
     {
         [ObservableProperty]
-        private DateTime _start = DateTime.Today;
+        private DateTime? _start = DateTime.Today;
 
         [ObservableProperty]
         private DateTime? _end;
 
         [ObservableProperty]
-        private string? _description;
+        [Required(ErrorMessageResourceType = typeof(Resources.Strings.Resources), ErrorMessageResourceName = nameof(Resources.Strings.Resources.GlobalError_PropertyRequired))]
+        private string? _name;
 
         private License? _license;
+
+        private User? _user;
+
+        private string? _licenseId;
 
         public LicenseViewModel(
             IDispatcherService dispatcherService,
             INavigationService navigationService,
             IDialogService dialogService,
-            ILogger<LicenseViewModel> logger)
+            ILogger<LicenseViewModel> logger,
+            IUserRepository userRepository,
+            IUserService userService)
             : base(dispatcherService, navigationService, dialogService, logger)
         {
+            UserRepository = userRepository;
+            UserService = userService;
         }
 
         public bool HasEnd
@@ -39,16 +54,109 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile.LicenseEditors
             }
         }
 
+        private IUserRepository UserRepository { get; }
+
+        private IUserService UserService { get; }
+
+        public override async Task OnNavigatedToAsync()
+        {
+            using (var worker = ScheduleWork())
+            {
+                try
+                {
+                    var user = await UserRepository.GetItemAsync(worker.Token).ConfigureAwait(false);
+                    var license = user?.Profile?.Licenses?.FirstOrDefault(x => x.Id == _licenseId);
+                    await ExecuteOnUIThreadAsync(() => LoadonUIThread(user, license), worker.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    Logger?.LogDebug($"Canceled {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
+                }
+                catch (ObjectDisposedException)
+                {
+                    Logger?.LogDebug($"Canceled {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
+                }
+                catch (Exception ex)
+                {
+                    Logger?.LogError(ex, $"Unknown error while {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
+                }
+                finally
+                {
+                    UnscheduleWork(worker);
+                }
+            }
+        }
+
+        protected override void OnPrepare(NavigationParameters navigationParameters)
+        {
+            _licenseId = navigationParameters.GetValue<string?>(NavigationParameter.Id);
+        }
+
+        protected override async Task<bool> SaveAsync(CancellationToken token)
+        {
+            if (_user == null || !IsDirty)
+            {
+                return !IsDirty;
+            }
+
+            token.ThrowIfCancellationRequested();
+            _user.Profile = _user.Profile ?? new UserProfile();
+            var license = _license ?? new License();
+
+            license.Name = Name ?? string.Empty;
+            license.Expires = End.ToDTODate();
+            license.Granted = Start.ToDTODate();
+
+            if (!_user.Profile.Licenses.Contains(license))
+            {
+                _user.Profile.Licenses.Add(license);
+            }
+
+            var response = await UserService.SaveAsync(_user, token).ConfigureAwait(false);
+            if (string.IsNullOrWhiteSpace(response))
+            {
+                Logger.LogError($"Unable to save user remotely {nameof(SaveAsync)} in {GetType().Name}.");
+                return !IsDirty;
+            }
+
+            _user.Id = response;
+            var userResult = await UserService.GetUserAsync(_user.Id, token).ConfigureAwait(false);
+            if (userResult == null || !await UserRepository.SaveAsync(userResult, CancellationToken.None).ConfigureAwait(false))
+            {
+                Logger.LogError($"Unable to save user locally {nameof(SaveAsync)} in {GetType().Name}.");
+                return !IsDirty;
+            }
+
+            IsDirty = false;
+            return !IsDirty;
+        }
+
         protected override void RefreshCommands()
         {
             base.RefreshCommands();
+            DeleteCommand?.NotifyCanExecuteChanged();
             ClearEndCommand?.NotifyCanExecuteChanged();
+        }
+
+        partial void OnNameChanging(string? value)
+        {
+            value = string.IsNullOrWhiteSpace(value) ? null : value;
+        }
+
+        partial void OnNameChanged(string? value)
+        {
+            ValidateProperty(value, nameof(Name));
+            IsDirty = true;
+        }
+
+        partial void OnStartChanged(DateTime? value)
+        {
+            IsDirty = true;
         }
 
         partial void OnEndChanged(DateTime? value)
         {
-            OnPropertyChanged(nameof(HasEnd));
-            RefreshCommands();
+            IsDirty = true;
         }
 
         [RelayCommand(CanExecute = nameof(CanClearEnd))]
@@ -60,6 +168,67 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile.LicenseEditors
         private bool CanClearEnd()
         {
             return !IsBusy && HasEnd;
+        }
+
+        private void LoadonUIThread(User? user, License? license)
+        {
+            _license = license;
+            _user = user;
+            Name = license?.Name;
+            Start = license?.Granted.ToUIDate();
+            End = license?.Expires.ToUIDate();
+            IsDirty = false;
+            ValidateCommand?.Execute(null);
+        }
+
+        [RelayCommand(AllowConcurrentExecutions = false, CanExecute = nameof(CanDelete))]
+        private async Task Delete(CancellationToken token)
+        {
+            using (var worker = ScheduleWork(token))
+            {
+                try
+                {
+                    _user!.Profile!.Licenses.Remove(_license!);
+                    var response = await UserService.SaveAsync(_user, worker.Token).ConfigureAwait(false);
+                    if (string.IsNullOrWhiteSpace(response))
+                    {
+                        Logger.LogError($"Unable to delete user remotely {nameof(Delete)} in {GetType().Name}.");
+                        await ShowErrorAsync(Resources.Strings.Resources.GlobalError_UnableToSaveData, worker.Token).ConfigureAwait(false);
+                        return;
+                    }
+
+                    if (!await UserRepository.SaveAsync(_user, CancellationToken.None).ConfigureAwait(false))
+                    {
+                        Logger.LogError($"Unable to save user locally {nameof(Delete)} in {GetType().Name}.");
+                        await ShowErrorAsync(Resources.Strings.Resources.GlobalError_UnableToSaveData, worker.Token).ConfigureAwait(false);
+                        return;
+                    }
+
+                    IsDirty = false;
+                    await NavigationService.PopAsync(worker.Token).ConfigureAwait(false);
+                }
+                catch (OperationCanceledException)
+                {
+                    Logger?.LogDebug($"Canceled {nameof(Delete)} in {GetType().Name}.");
+                }
+                catch (ObjectDisposedException)
+                {
+                    Logger?.LogDebug($"Canceled {nameof(Delete)} in {GetType().Name}.");
+                }
+                catch (Exception ex)
+                {
+                    Logger?.LogError(ex, $"Unknown error in {nameof(Delete)} in {GetType().Name}.");
+                }
+                finally
+                {
+                    UnscheduleWork(worker);
+                }
+            }
+        }
+
+        private bool CanDelete()
+        {
+            return !IsBusy && _license != null;
         }
     }
 }
