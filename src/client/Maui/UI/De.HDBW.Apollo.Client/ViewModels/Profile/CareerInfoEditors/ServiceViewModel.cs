48a072ad0efbbb896014993fe16ced8c2e308873
Lars Heinrich <lh@heires.net>
2024-01-24 16:45:24 +0100

    improved ServiceViewModel

--- src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/CareerInfoEditors/ServiceViewModel.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/CareerInfoEditors/ServiceViewModel.cs
@@ -3,28 +3,20 @@
 
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using De.HDBW.Apollo.Client.Contracts;
 using De.HDBW.Apollo.Client.Models.Interactions;
+using De.HDBW.Apollo.SharedContracts.Repositories;
+using De.HDBW.Apollo.SharedContracts.Services;
 using Invite.Apollo.App.Graph.Common.Models.UserProfile;
 using Invite.Apollo.App.Graph.Common.Models.UserProfile.Enums;
 using Microsoft.Extensions.Logging;
 
 namespace De.HDBW.Apollo.Client.ViewModels.Profile.CareerInfoEditors
 {
-    public partial class ServiceViewModel : BaseViewModel
+    public partial class ServiceViewModel : BasicViewModel
     {
         [ObservableProperty]
-        private DateTime _start = DateTime.Today;
-
-        [ObservableProperty]
-        private DateTime? _end;
-
-        [ObservableProperty]
         private string? _occupationName;
-
-        [ObservableProperty]
-        private string? _description;
 
         [ObservableProperty]
         private string? _nameOfInstitution;
@@ -41,84 +33,64 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile.CareerInfoEditors
         [ObservableProperty]
         private InteractionEntry? _selectedServiceType;
 
-        private CareerInfo? _career;
-
         public ServiceViewModel(
             IDispatcherService dispatcherService,
             INavigationService navigationService,
             IDialogService dialogService,
-            ILogger<ServiceViewModel> logger)
-            : base(dispatcherService, navigationService, dialogService, logger)
+            ILogger<ServiceViewModel> logger,
+            IUserRepository userRepository,
+            IUserService userService)
+            : base(dispatcherService, navigationService, dialogService, logger, userRepository, userService)
         {
         }
 
-        public bool HasEnd
+        protected override async Task<CareerInfo?> LoadDataAsync(User user, string? entryId, CancellationToken token)
         {
-            get
-            {
-                return End.HasValue;
-            }
+            token.ThrowIfCancellationRequested();
+
+            var serviceTypes = new List<InteractionEntry>();
+            serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_CivilianService, ServiceType.CivilianService, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
+            serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_MilitaryService, ServiceType.MilitaryService, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
+            serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_VoluntaryMilitaryService, ServiceType.VoluntaryMilitaryService, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
+            serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_MilitaryExercise, ServiceType.MilitaryExercise, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
+            var currentData = await base.LoadDataAsync(user, entryId, token).ConfigureAwait(false);
+            await ExecuteOnUIThreadAsync(() => LoadonUIThread(currentData, serviceTypes), token).ConfigureAwait(false);
+            return currentData;
         }
 
-        public override async Task OnNavigatedToAsync()
+        protected override void ApplyChanges(CareerInfo entity)
         {
-            using (var worker = ScheduleWork())
-            {
-                try
-                {
-                    var serviceTypes = new List<InteractionEntry>();
-                    serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_CivilianService, ServiceType.CivilianService, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
-                    serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_MilitaryService, ServiceType.MilitaryService, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
-                    serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_VoluntaryMilitaryService, ServiceType.VoluntaryMilitaryService, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
-                    serviceTypes.Add(InteractionEntry.Import(Resources.Strings.Resources.ServiceType_MilitaryExercise, ServiceType.MilitaryExercise, (x) => { return Task.CompletedTask; }, (x) => { return true; }));
-                    await ExecuteOnUIThreadAsync(() => LoadonUIThread(serviceTypes), worker.Token);
-                }
-                catch (OperationCanceledException)
-                {
-                    Logger?.LogDebug($"Canceled {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
-                }
-                catch (ObjectDisposedException)
-                {
-                    Logger?.LogDebug($"Canceled {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
-                }
-                catch (Exception ex)
-                {
-                    Logger?.LogError(ex, $"Unknown error while {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
-                }
-                finally
-                {
-                    UnscheduleWork(worker);
-                }
-            }
+            base.ApplyChanges(entity);
+            entity.City = City;
+            entity.Country = Country;
+            entity.NameOfInstitution = NameOfInstitution;
+            entity.ServiceType = (SelectedServiceType?.Data as ServiceType?) ?? ServiceType.Unknown;
         }
 
-        protected override void RefreshCommands()
+        partial void OnCityChanged(string? value)
         {
-            base.RefreshCommands();
-            ClearEndCommand?.NotifyCanExecuteChanged();
+            this.IsDirty = true;
         }
 
-        partial void OnEndChanged(DateTime? value)
+        partial void OnCountryChanged(string? value)
         {
-            OnPropertyChanged(nameof(HasEnd));
-            RefreshCommands();
+            this.IsDirty = true;
         }
 
-        [RelayCommand(CanExecute = nameof(CanClearEnd))]
-        private void ClearEnd()
+        partial void OnNameOfInstitutionChanged(string? value)
         {
-            End = null;
+            this.IsDirty = true;
         }
 
-        private bool CanClearEnd()
+        partial void OnSelectedServiceTypeChanged(InteractionEntry? value)
         {
-            return !IsBusy && HasEnd;
+            this.IsDirty = true;
         }
 
-        private void LoadonUIThread(List<InteractionEntry> serviceTypes)
+        private void LoadonUIThread(CareerInfo? careerInfo, List<InteractionEntry> serviceTypes)
         {
             ServiceTypes = new ObservableCollection<InteractionEntry>(serviceTypes);
-            SelectedServiceType = ServiceTypes.FirstOrDefault();
+            SelectedServiceType = ServiceTypes.FirstOrDefault(x => (x.Data as ServiceType?) == careerInfo?.ServiceType) ?? ServiceTypes.FirstOrDefault();
         }
     }
 }
