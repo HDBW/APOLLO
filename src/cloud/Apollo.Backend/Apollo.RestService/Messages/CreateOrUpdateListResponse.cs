17acbdd1b5ae15515fc139a310d8fbe46973fdcb
Mukit Khan <mkhan@daenet.com>
2024-01-26 12:52:45 +0100

    Add  List controller

---
+++ src/cloud/Apollo.Backend/Apollo.RestService/Messages/CreateOrUpdateListResponse.cs
@@ -0,0 +1,18 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using Apollo.Common.Entities;
+
+namespace Apollo.RestService.Messages
+{
+    /// <summary>
+    /// Defines the response message of the CreateOrUpdateTraining operation.
+    /// </summary>
+    public class CreateOrUpdateListResponse
+    {
+        /// <summary>
+        /// The Id of the created training instance.
+        /// </summary>
+        public List? List{ get; set; }
+    }
+}
