a615cf6f5818931e3063120d3191c28bcd776eee
Lars Heinrich <lh@heires.net>
2024-01-26 09:58:13 +0100

    improved EducationInfoEditViewModel

--- src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/EducationInfoEditViewModel.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/EducationInfoEditViewModel.cs
@@ -2,18 +2,22 @@
 // The HDBW licenses this file to you under the MIT license.
 
 using System.Collections.ObjectModel;
+using Android.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using De.HDBW.Apollo.Client.Contracts;
 using De.HDBW.Apollo.Client.Dialogs;
 using De.HDBW.Apollo.Client.Models;
+using De.HDBW.Apollo.Client.Models.Profile;
+using De.HDBW.Apollo.SharedContracts.Repositories;
+using De.HDBW.Apollo.SharedContracts.Services;
 using Invite.Apollo.App.Graph.Common.Models.UserProfile;
 using Invite.Apollo.App.Graph.Common.Models.UserProfile.Enums;
 using Microsoft.Extensions.Logging;
 
 namespace De.HDBW.Apollo.Client.ViewModels.Profile
 {
-    public partial class EducationInfoEditViewModel : BaseViewModel
+    public partial class EducationInfoEditViewModel : AbstractListViewModel<EducationInfoEntry, EducationInfo>
     {
         [ObservableProperty]
         private ObservableCollection<EducationInfo> _educations = new ObservableCollection<EducationInfo>();
@@ -22,19 +26,61 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile
             IDispatcherService dispatcherService,
             INavigationService navigationService,
             IDialogService dialogService,
-            ILogger<EducationInfoEditViewModel> logger)
-            : base(dispatcherService, navigationService, dialogService, logger)
+            ILogger<EducationInfoEditViewModel> logger,
+            IUserRepository userRepository,
+            IUserService userService)
+            : base(dispatcherService, navigationService, dialogService, logger, userRepository, userService, Routes.EmptyView)
         {
         }
 
-        protected override void RefreshCommands()
+        public override async Task OnNavigatedToAsync()
         {
-            AddCommand?.NotifyCanExecuteChanged();
-            base.RefreshCommands();
+            using (var worker = ScheduleWork())
+            {
+                try
+                {
+                    User = await UserRepository.GetItemAsync(worker.Token).ConfigureAwait(false);
+                    var items = new List<EducationInfo>();
+                    items.AddRange(User?.Profile?.EducationInfos ?? new List<EducationInfo>());
+                    items = items.OrderBy(x => x.Start.Ticks).ToList();
+                    await ExecuteOnUIThreadAsync(
+                        () => LoadonUIThread(items.Select(x => EducationInfoEntry.Import(x, EditAsync, CanEdit, DeleteAsync, CanDelete))), worker.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    Logger?.LogDebug($"Canceled {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
+                }
+                catch (ObjectDisposedException)
+                {
+                    Logger?.LogDebug($"Canceled {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
+                }
+                catch (Exception ex)
+                {
+                    Logger?.LogError(ex, $"Unknown error while {nameof(OnNavigatedToAsync)} in {GetType().Name}.");
+                }
+                finally
+                {
+                    UnscheduleWork(worker);
+                }
+            }
         }
 
-        [RelayCommand(AllowConcurrentExecutions = false, CanExecute = nameof(CanAdd))]
-        private async Task Add(CancellationToken token)
+        protected override string? GetIdFromItem(AbstractProfileEntry<EducationInfo> entry)
+        {
+            return entry.Export().Id;
+        }
+
+        protected override string? GetEditRouteFromItem(AbstractProfileEntry<EducationInfo> entry)
+        {
+            return GetRoute(entry.Export().EducationType);
+        }
+
+        protected override void RemoveItemFromUser(User user, AbstractProfileEntry<EducationInfo> entry)
+        {
+            user.Profile!.EducationInfos.Remove(entry.Export());
+        }
+
+        protected override async Task Add(CancellationToken token)
         {
             using (var worker = ScheduleWork(token))
             {
@@ -45,32 +91,10 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile
                     var result = await DialogService.ShowPopupAsync<SelectOptionDialog, NavigationParameters, NavigationParameters>(parameters, worker.Token);
                     if (result?.GetValue<bool?>(NavigationParameter.Result) ?? false)
                     {
-                        var route = string.Empty;
                         var educationType = result?.GetValue<EducationType?>(NavigationParameter.Data);
                         parameters = new NavigationParameters();
                         parameters.Add(NavigationParameter.Type, educationType ?? EducationType.Unkown);
-                        switch (educationType)
-                        {
-                            case EducationType.Education:
-                                route = Routes.EducationInfoEducationView;
-                                break;
-                            case EducationType.CompanyBasedVocationalTraining:
-                                route = Routes.EducationInfoCompanyBasedVocationalTrainingView;
-                                break;
-                            case EducationType.Study:
-                                route = Routes.EducationInfoStudyView;
-                                break;
-                            case EducationType.VocationalTraining:
-                                route = Routes.EducationInfoVocationalTrainingView;
-                                break;
-                            case EducationType.FurtherEducation:
-                                route = Routes.EducationInfoFurtherEducationView;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        await NavigationService.NavigateAsync(route, worker.Token, parameters);
+                        await NavigationService.NavigateAsync(GetRoute(educationType), worker.Token, parameters);
                     }
                 }
                 catch (OperationCanceledException)
@@ -92,9 +116,31 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile
             }
         }
 
-        private bool CanAdd()
+        private string GetRoute(EducationType? educationType)
         {
-            return !IsBusy;
+            var route = string.Empty;
+            switch (educationType)
+            {
+                case EducationType.Education:
+                    route = Routes.EducationInfoEducationView;
+                    break;
+                case EducationType.CompanyBasedVocationalTraining:
+                    route = Routes.EducationInfoCompanyBasedVocationalTrainingView;
+                    break;
+                case EducationType.Study:
+                    route = Routes.EducationInfoStudyView;
+                    break;
+                case EducationType.VocationalTraining:
+                    route = Routes.EducationInfoVocationalTrainingView;
+                    break;
+                case EducationType.FurtherEducation:
+                    route = Routes.EducationInfoFurtherEducationView;
+                    break;
+                default:
+                    break;
+            }
+
+            return route;
         }
     }
 }
