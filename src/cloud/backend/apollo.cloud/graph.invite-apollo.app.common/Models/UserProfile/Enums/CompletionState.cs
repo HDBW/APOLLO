6e25d476c3bd732c66827f4b7312b6c1269f4d03
Lars Heinrich <lh@heires.net>
2024-02-08 19:26:54 +0100

    fixed other typo

--- src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/UserProfile/Enums/CompletionState.cs
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/UserProfile/Enums/CompletionState.cs
@@ -8,6 +8,6 @@ namespace Invite.Apollo.App.Graph.Common.Models.UserProfile.Enums
         None = 0,
         Completed = 1,
         Failed = 2,
-        Ongoning = 3,
+        Ongoing = 3,
     }
 }
