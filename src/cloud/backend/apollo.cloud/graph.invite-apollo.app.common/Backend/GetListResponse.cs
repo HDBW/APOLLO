d3fc9575e6770c96509f0db216e4ca1ee5f11fee
Lars Heinrich <lh@heires.net>
2024-02-08 15:47:06 +0100

    Added List Models

---
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/GetListResponse.cs
@@ -0,0 +1,15 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using Invite.Apollo.App.Graph.Common.Models.Lists;
+
+namespace Invite.Apollo.App.Graph.Common.Backend.Api
+{
+    public class GetListResponse
+    {
+        /// <summary>
+        /// The set of list items.
+        /// </summary>
+        public ApolloList Result { get; set; }
+    }
+}
---
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Models/Lists/ApolloListItem.cs
@@ -0,0 +1,33 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+namespace Invite.Apollo.App.Graph.Common.Models.Lists
+{
+    /// <summary>
+    ///  The item stored in the DB.
+    ///  Describes the item in the list in the specific language.
+    /// </summary>
+    public class ApolloListItem
+    {
+        /// <summary>
+        /// Identifier of the item in the list.
+        /// </summary>
+        public int ListItemId { get; set; }
+
+        /// <summary>
+        /// The ISO language code.
+        /// </summary>
+        public string? Lng { get; set; }
+
+        /// <summary>
+        /// Optionl description of the values.
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// The name of the item.
+        /// </summary>
+        public string Value { get; set; }
+
+    }
+}
