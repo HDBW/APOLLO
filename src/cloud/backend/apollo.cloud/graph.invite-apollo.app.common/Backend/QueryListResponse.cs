1f952dc6cbb921eeff4e3326cc2c97c03fe1a570
Lars Heinrich <lh@heires.net>
2024-02-08 18:21:06 +0100

    Moving Target List Requests

--- src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/QueryListResponse.cs
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/QueryListResponse.cs
@@ -1,15 +1,16 @@
 ﻿// (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
+using System.Collections.Generic;
 using Invite.Apollo.App.Graph.Common.Models.Lists;
 
 namespace Invite.Apollo.App.Graph.Common.Backend.Api
 {
-    public class GetListResponse
+    public class QueryListResponse
     {
         /// <summary>
         /// The set of list items.
         /// </summary>
-        public ApolloList Result { get; set; }
+        public List<ApolloListItem> Result { get; set; }
     }
 }
