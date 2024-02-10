2bad90d5adaa888e3a4f3df65e767ff470091593
Patric Boscolo <patric@imenja.io>
2024-02-10 03:07:28 +0100

    Fixing Qualification

--- src/cloud/Apollo.Backend/Apollo.Service.Common/Entities/Qualification.cs
+++ src/cloud/Apollo.Backend/Apollo.Service.Common/Entities/Qualification.cs
@@ -11,16 +11,18 @@ namespace Apollo.Common.Entities
         /// <summary>
         /// Identifier of the item in the list.
         /// </summary>
-        public string Id { get; set; }
+        public string? Id { get; set; }
 
         // Qualification_Description_filtered.txt
         // FreiText
+        public string Name { get; set; }
+        
         public string? Description { get; set; }
 
         public DateTime? IssueDate { get; set; }
 
         public DateTime? ExpirationDate { get; set; }
 
-        public string IssuingAuthority { get; set; }
+        public string? IssuingAuthority { get; set; }
     }
 }
