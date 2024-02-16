6e8acba4e1599ce6b85766294a4ff8e8c7e26b9d
Lars Heinrich <lh@heires.net>
2024-02-16 10:09:45 +0100

    added GetTrainingResponse

---
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/GetTrainingResponse.cs
@@ -0,0 +1,12 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using Invite.Apollo.App.Graph.Common.Models.Trainings;
+
+namespace Invite.Apollo.App.Graph.Common.Backend.Api
+{
+    public class GetTrainingResponse
+    {
+        public Training? Training { get; set; }
+    }
+}
