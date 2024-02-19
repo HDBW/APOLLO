7ef67b2f09e03dafa8140592e7f64e4628819705
Lars Heinrich <lh@heires.net>
2024-02-19 16:41:06 +0100

    wip

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Models/Training/LoanItem.cs
@@ -0,0 +1,45 @@
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+using Invite.Apollo.App.Graph.Common.Models.Trainings;
+using Contact = Invite.Apollo.App.Graph.Common.Models.Contact;
+
+namespace De.HDBW.Apollo.Client.Models.Training
+{
+    public partial class LoanItem : ContactItem
+    {
+        private LoanItem(
+            Loans loan,
+            Func<Uri?, CancellationToken, Task>? openUrlHandler,
+            Func<Uri?, bool>? canOpenUrlHandler,
+            Contact contact,
+            Func<string?, CancellationToken, Task>? openMailHandler,
+            Func<string?, bool>? canOpenMailHandler,
+            Func<string?, CancellationToken, Task>? openDailerHandler,
+            Func<string?, bool>? canOpenDailerHandler)
+            : base(loan.Name, contact, openMailHandler, canOpenMailHandler, openDailerHandler, canOpenDailerHandler)
+        {
+            if (!string.IsNullOrWhiteSpace(loan.Description))
+            {
+                Items.Insert(0, LineItem.Import(null, loan.Description));
+            }
+
+            if (loan.Url != null)
+            {
+                Items.Add(InteractiveLineItem.Import(KnonwIcons.Web, loan.Url.OriginalString));
+            }
+        }
+
+        public static LoanItem Import(
+            Loans loan,
+            Func<Uri?, CancellationToken, Task>? openUrlHandler,
+            Func<Uri?, bool>? canOpenUrlHandler,
+            Func<string?, CancellationToken, Task>? openMailHandler,
+            Func<string?, bool>? canOpenMailHandler,
+            Func<string?, CancellationToken, Task>? openDailerHandler,
+            Func<string?, bool>? canOpenDailerHandler)
+        {
+            return new LoanItem(loan, openUrlHandler, canOpenUrlHandler, loan.LoanContact ?? new Contact(), openMailHandler, canOpenMailHandler, openDailerHandler, canOpenDailerHandler);
+        }
+    }
+}
