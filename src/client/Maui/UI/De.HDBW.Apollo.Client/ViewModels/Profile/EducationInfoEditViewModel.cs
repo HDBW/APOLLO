bac64eb9cf5ad88eaf166e009db212e35718f360
Larhei <47631685+Larhei@users.noreply.github.com>
2024-02-19 17:36:59 +0100

    fixed hittesting in SelectoptionDialog

--- src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/EducationInfoEditViewModel.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/ViewModels/Profile/EducationInfoEditViewModel.cs
@@ -35,6 +35,11 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile
 
         public override async Task OnNavigatedToAsync()
         {
+            if (IsShowingDialog)
+            {
+                return;
+            }
+
             using (var worker = ScheduleWork())
             {
                 try
@@ -81,6 +86,7 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile
             {
                 try
                 {
+                    IsShowingDialog = true;
                     var parameters = new NavigationParameters();
                     parameters.Add(NavigationParameter.Data, EducationType.Unknown);
                     var result = await DialogService.ShowPopupAsync<SelectOptionDialog, NavigationParameters, NavigationParameters>(parameters, worker.Token);
@@ -106,6 +112,7 @@ namespace De.HDBW.Apollo.Client.ViewModels.Profile
                 }
                 finally
                 {
+                    IsShowingDialog = false;
                     UnscheduleWork(worker);
                 }
             }
