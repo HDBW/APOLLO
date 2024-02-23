9f1ba79b7874e22572457fb1d054a670e72d790f
Lars Heinrich <lh@heires.net>
2024-02-23 14:47:16 +0100

    Added Messages, fixed typo

---
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/CreateOrUpdateProfileResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Invite.Apollo.App.Graph.Common.Backend.Api
+{
+    // Define a response message for creating or updating a user.
+    public class CreateOrUpdateProfileResponse
+    {
+
+        /// <summary>
+        ///  The identifier of the profile.
+        /// </summary>
+        public string Id { get; internal set; }
+    }
+}
