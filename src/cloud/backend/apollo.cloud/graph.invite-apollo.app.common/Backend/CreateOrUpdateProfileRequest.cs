5a8a1b536658f9c755f6921b9f524e2a80052e44
Lars Heinrich <lh@heires.net>
2024-02-23 15:10:22 +0100

    Cleanup

--- src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/CreateOrUpdateProfileRequest.cs
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/CreateOrUpdateProfileRequest.cs
@@ -5,23 +5,20 @@ using Invite.Apollo.App.Graph.Common.Models.UserProfile;
 
 namespace Invite.Apollo.App.Graph.Common.Backend.Api
 {
-    namespace Apollo.RestService.Apollo.Common.Messages
+    /// <summary>
+    /// Defines the request for creating or updating a profile.
+    /// </summary>
+    public class CreateOrUpdateProfileRequest
     {
         /// <summary>
-        /// Defines the request for creating or updating a profile.
+        /// The Id of the user to whom the profile belongs.
         /// </summary>
-        public class CreateOrUpdateProfileRequest
-        {
-            /// <summary>
-            /// The Id of the user to whom the profile belongs.
-            /// </summary>
-            public string UserId { get; set; }
+        public string UserId { get; set; }
 
-            /// <summary>
-            ///  Property to hold the profile to be created or updated
-            /// </summary>
-            public Profile Profile { get; set; }
+        /// <summary>
+        ///  Property to hold the profile to be created or updated
+        /// </summary>
+        public Profile Profile { get; set; }
 
-        }
     }
 }
--- src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/CreateOrUpdateProfileResponse.cs
+++ src/cloud/backend/apollo.cloud/graph.invite-apollo.app.common/Backend/CreateOrUpdateProfileResponse.cs
@@ -1,4 +1,7 @@
-﻿namespace Invite.Apollo.App.Graph.Common.Backend.Api
+﻿// (c) Licensed to the HDBW under one or more agreements.
+// The HDBW licenses this file to you under the MIT license.
+
+namespace Invite.Apollo.App.Graph.Common.Backend.Api
 {
     // Define a response message for creating or updating a user.
     public class CreateOrUpdateProfileResponse
