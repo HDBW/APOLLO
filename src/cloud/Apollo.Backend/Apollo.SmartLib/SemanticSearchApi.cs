e3fd68dc8b8e2c95265ef01d7863f9e0f026e9ba
Damir Dobric <ddobric@daenet.com>
2024-02-26 10:49:05 +0100

    Added reference to internal NUGET feed. Authemntication might be required!

---
+++ src/cloud/Apollo.Backend/Apollo.SmartLib/SemanticSearchApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo.SmartLib
+{
+    /// <summary>
+    /// API that implements all semantic related functionalities.
+    /// </summary>
+    public class SemanticSearchApi
+    {
+    }
+}
