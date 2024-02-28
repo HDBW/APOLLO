06a70d1050ac50c6c47e4afc9bae6ad71769b657
Larhei <47631685+Larhei@users.noreply.github.com>
2024-02-28 14:29:23 +0100

    disabled xaml compilation for TrainingContentView

--- src/client/Maui/UI/De.HDBW.Apollo.Client/Views/Training/TrainingContentView.xaml.cs
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/Training/TrainingContentView.xaml.cs
@@ -1,11 +1,10 @@
 // (c) Licensed to the HDBW under one or more agreements.
 // The HDBW licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using De.HDBW.Apollo.Client.ViewModels.Training;
 
 namespace De.HDBW.Apollo.Client.Views.Training;
-
+[XamlCompilation(XamlCompilationOptions.Skip)]
 public partial class TrainingContentView : ContentPage
 {
     public TrainingContentView(TrainingContentViewModel model)
