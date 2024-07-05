790d400e29c8e746231bace0f169430e4df2a42f
thewen-heires <75784197+thewen-heires@users.noreply.github.com>
2024-07-05 10:41:55 +0200

    added ResultDetailSheet, ResultOverView

---
+++ src/client/Maui/UI/De.HDBW.Apollo.Client/Views/Assessments/ResultDetailSheet.xaml.cs
@@ -0,0 +1,11 @@
+namespace De.HDBW.Apollo.Client.Views.Assessments
+{
+    [XamlCompilation(XamlCompilationOptions.Skip)]
+    public partial class ResultDetailSheet
+    {
+        public ResultDetailSheet()
+        {
+            InitializeComponent();
+        }
+    }
+}
